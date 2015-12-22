d365748b8b7464b5b5c9c15183c9a3b7509bf12d
Maciej Sypień <maciejsypien@gmail.com>
2015-12-22 16:16:15 +0100

    add partials

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,15 +1,15 @@
 angular.module('profitelo.controller.home', [
-  'ui.router'
-])
-.config(function($stateProvider) {
-  $stateProvider.state('app.home', {
-    url: '/home',
-    controllerAs: 'vm',
-    controller: 'HomeController',
-    templateUrl: 'home/home.tpl.html'
-  });
-})
-.controller('HomeController', HomeController);
+    'ui.router'
+  ])
+  .config(function ($stateProvider) {
+    $stateProvider.state('app.home', {
+      url: '/home',
+      controllerAs: 'vm',
+      controller: 'HomeController',
+      templateUrl: 'home/home.tpl.html'
+    });
+  })
+  .controller('HomeController', HomeController);
 
 function HomeController() {
   var vm = this;
