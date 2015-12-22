01812786fd86bfc58430cc9e7b05113672402c60
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2015-12-22 13:06:51 +0100

    tests options, gulp code reformat

--- gulp_src/build-styles.js
+++ gulp_src/build-styles.js
@@ -1,21 +1,21 @@
-var pkg     = require('../package.json');
-var config  = require('../build.config');
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var rename  = require('gulp-rename');
-var concat  = require('gulp-concat');
-var addSrc  = require('gulp-add-src');
-var gulpif  = require('gulp-if');
+var pkg = require('../package.json');
+var config = require('../build.config');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var addSrc = require('gulp-add-src');
+var gulpif = require('gulp-if');
 var connect = require('gulp-connect');
 
-gulp.task('build-styles', function() {
-    return gulp.src('src/template/' + config.project_theme_name + "/sass/main.sass")
-        .pipe(sass({
-            indentedSyntax: true
-        }).on('error', sass.logError))
-        .pipe(addSrc(config.vendor_files.css))
-        .pipe(concat(pkg.name + '-' + pkg.version + '.css'))
-        .pipe(gulp.dest(config.compile_dir))
-        .pipe(connect.reload());
+gulp.task('build-styles', function () {
+  return gulp.src('src/template/' + config.project_theme_name + "/sass/main.sass")
+    .pipe(sass({
+      indentedSyntax: true
+    }).on('error', sass.logError))
+    .pipe(addSrc(config.vendor_files.css))
+    .pipe(concat(pkg.name + '-' + pkg.version + '.css'))
+    .pipe(gulp.dest(config.compile_dir))
+    .pipe(connect.reload());
 });
 
--- gulp_src/transpile-js.js
+++ gulp_src/transpile-js.js
@@ -1,27 +1,27 @@
-var config      = require('../build.config');
+var config = require('../build.config');
 
-var gulp        = require('gulp');
-var babel       = require('gulp-babel');
-var annotate    = require('gulp-ng-annotate');
-var plumber     = require('gulp-plumber');
-var cache       = require('gulp-cached');
-var connect     = require('gulp-connect');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var annotate = require('gulp-ng-annotate');
+var plumber = require('gulp-plumber');
+var cache = require('gulp-cached');
+var connect = require('gulp-connect');
 
 
-gulp.task('transpile-scripts', function() {
+gulp.task('transpile-scripts', function () {
 
-    return gulp.src(config.app_files.js)
-        .pipe(plumber(function(error) {
-            if (config.variables.production) {
-                throw new Error(error.message);
-            }
-        }))
-        .pipe(cache('coffeeBuild'))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(annotate())
-        .pipe(gulp.dest(config.compile_dir))
-        .pipe(connect.reload());
+  return gulp.src(config.app_files.js)
+    .pipe(plumber(function (error) {
+      if (config.variables.production) {
+        throw new Error(error.message);
+      }
+    }))
+    .pipe(cache('coffeeBuild'))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(annotate())
+    .pipe(gulp.dest(config.compile_dir))
+    .pipe(connect.reload());
 
 });
