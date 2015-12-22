5541e9c4981261a4288d225d85a7c43aa36b03ed
Jakub Pęksa <jakub.peksa@itelo.pl>
2015-12-22 08:53:34 +0100

    jade watch

--- gulp_src/watchers.js
+++ gulp_src/watchers.js
@@ -25,34 +25,34 @@ gulp.task('watchers', function() {
         return runSequence(jsWatchArgs.tasks);
     });
 
-    //var jadeWatcher = gulp.watch(config.build.app_files.jade_all_templates, function() {
-    //    return runSequence('compile-jade');
-    //});
+    var jadeWatcher = gulp.watch(buildConfig.app_files.jade_all, function() {
+        return runSequence('compile-jade');
+    });
 
     //var sassWatcher = gulp.watch(config.build.app_files.sass_all, function() {
     //    return runSequence('build-styles');
     //});
 
     jsWatcher.on('change', function(e) {
-        return console.log(chalk.yellow('[COFFEE] ' + chalk.yellow(e.path)));
+        return console.log(chalk.yellow('[JS] ' + chalk.yellow(e.path)));
     });
 
     jsWatcher.on('add', function(e) {
         setTimeout(function() {
             return runSequence('html-injector');
         }, 1000);
-        return console.log(chalk.yellow('[NEW COFFEE FILE INJECTED] ' + chalk.yellow(e)));
+        return console.log(chalk.yellow('[NEW JS FILE INJECTED] ' + chalk.yellow(e)));
     });
 
     //sassWatcher.on('change', function(e) {
     //    return console.log(chalk.yellow('[SASS] ' + chalk.yellow(e.path)));
     //});
 
-    //jadeWatcher.on('change', function(e) {
-    //    console.log(chalk.yellow('[JADE] ' + chalk.yellow(e.path)));
-    //    if (e.path.match(/partial.jade/)) {
-    //        arguments.jadeCache = false;
-    //        return console.log(chalk.blue('[JADE] Partial file had been modified. Running JADE without cache'));
-    //    }
-    //});
+    jadeWatcher.on('change', function(e) {
+        console.log(chalk.yellow('[JADE] ' + chalk.yellow(e.path)));
+        if (e.path.match(/partial.jade/)) {
+            arguments.jadeCache = false;
+            return console.log(chalk.blue('[JADE] Partial file had been modified. Running JADE without cache'));
+        }
+    });
 });
