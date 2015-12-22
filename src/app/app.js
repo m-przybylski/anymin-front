9e12cb33a116196ece19792b32e9459ccf859ce9
Maciej Sypień <maciejsypien@gmail.com>
2015-12-22 18:36:54 +0100

    add sample expert-profile

--- src/app/app.js
+++ src/app/app.js
@@ -1,28 +1,27 @@
 angular.module('profitelo', [
-  'templates-module',
-  'profitelo.controller.dashboard',
-  'profitelo.controller.home'
+    'templates-module',
+    'profitelo.controller.dashboard',
+    'profitelo.controller.home',
+    'profitelo.controller.expert-profile'
 ])
 
 
 .config(($urlRouterProvider, $stateProvider) => {
-  $stateProvider.state('app', {
-    url: '',
-    abstract: true,
-    controller: 'AppController',
-    templateUrl: 'templates/app.tpl.html'
-  });
-  $urlRouterProvider
-    .when('', '/')
-    .when('/', '/home');
+    $stateProvider.state('app', {
+        url: '',
+        abstract: true,
+        controller: 'AppController',
+        templateUrl: 'templates/app.tpl.html'
+    });
+    $urlRouterProvider
+        .when('', '/')
+        .when('/', '/home');
 })
 
 .controller('AppController', AppController);
 
-
 function AppController($scope) {
-  var vm = this;
+    var vm = this;
 
-
-  return vm;
+    return vm;
 }
---
+++ src/app/expert-profile/expert-profile.js
@@ -0,0 +1,21 @@
+angular.module('profitelo.controller.expert-profile', [
+    'ui.router'
+  ])
+  .config(function ($stateProvider) {
+    $stateProvider.state('app.expert-profile', {
+      url: '/expert-profile',
+      controllerAs: 'vm',
+      controller: 'ExpertProfileController',
+      templateUrl: 'expert-profile/expert-profile.tpl.html'
+    });
+  })
+  .controller('ExpertProfileController', ExpertProfileController);
+
+function ExpertProfileController() {
+  var vm = this;
+
+  console.log('Expert Profile');
+
+  return vm;
+
+}
