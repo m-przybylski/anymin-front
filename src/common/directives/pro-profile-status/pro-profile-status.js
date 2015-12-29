36bc220199e9241ef3435cb944e8a03ea6ae993f
Maciej Sypień <maciejsypien@gmail.com>
2015-12-29 17:34:42 +0100

    add pro-profile-status directive

---
+++ src/common/directives/pro-profile-status/pro-profile-status.js
@@ -0,0 +1,27 @@
+angular.module('profitelo.directive.pro-profile-status', [])
+
+.directive('proProfileStatus', () => {
+  return {
+    restrict:     'E',
+    replace:      true,
+    templateUrl:  'directives/pro-profile-status/pro-profile-status.tpl.html',
+    scope: {
+      status:     '=' // `on/off` or `true/false`
+    },
+    link: function($scope) {
+      if ($scope.status === 'undefined' || !$scope.status || $scope.status === 'off') {
+        $scope.status= 'off'
+      } else {
+        $scope.status= 'on'
+      }
+
+      $scope.colorClass = function(status) {
+        if (status === 'on') {
+          return 'text-success'
+        } else {
+          return 'text-danger'
+        }
+      }
+    }
+  }
+})
