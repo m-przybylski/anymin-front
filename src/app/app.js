431e422f225da49b916f2389ccb191f956df4b5e
Maciej Sypień <maciejsypien@gmail.com>
2015-12-29 14:42:45 +0100

    add angular sample flash messages (toastr)

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,8 @@
 angular.module('profitelo', [
   'pascalprecht.translate',
   'tmh.dynamicLocale',
+  'ngAnimate',
+  'toastr',  // some parts depends on ngAnimate
 
   // templates
   'templates-module',
@@ -30,7 +32,7 @@ angular.module('profitelo', [
 ])
 
 
-.config(($urlRouterProvider, $stateProvider, $translateProvider, tmhDynamicLocaleProvider) => {
+.config(($urlRouterProvider, $stateProvider, $translateProvider, tmhDynamicLocaleProvider, toastrConfig) => {
   $stateProvider.state('app', {
     url: '',
     abstract: true,
@@ -40,6 +42,36 @@ angular.module('profitelo', [
   $urlRouterProvider
     .when('', '/')
     .when('/', '/home');
+
+  angular.extend(toastrConfig, {
+    newestOnTop: true,
+    positionClass: 'toast-bottom-right',
+
+    allowHtml: false,
+    closeButton: false,
+    closeHtml: '<button>&times;</button>',
+    extendedTimeOut: 1000,
+    iconClasses: {
+      error: 'toast-error',
+      info: 'toast-info',
+      success: 'toast-success',
+      warning: 'toast-warning'
+    },
+    messageClass: 'toast-message',
+    onHidden: null,
+    onShown: null,
+    onTap: null,
+    progressBar: false,
+    tapToDismiss: true,
+    templates: {
+      toast: 'directives/toast/toast.html',
+      progressbar: 'directives/progressbar/progressbar.html'
+    },
+    timeOut: 5000,
+    titleClass: 'toast-title',
+    toastClass: 'toast'
+  });
+
 
   /**
    * Translations (angular translate)
