e0c492327fec1dda689fa0d6d2cd794933928876
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-01-04 14:03:37 +0100

    improve test runner

--- gulp_src/test-runner.js
+++ gulp_src/test-runner.js
@@ -1,101 +1,105 @@
-var config = require('../build.config');
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var plumber = require('gulp-plumber');
-var gulpif = require('gulp-if');
-var karma = require('gulp-karma');
-var babel       = require('gulp-babel');
+var config = require('../build.config')
+var gulp = require('gulp')
+var runSequence = require('run-sequence')
+var plumber = require('gulp-plumber')
+var karma = require('gulp-karma')
+var babel = require('gulp-babel')
 
 var karmaSettings = {
-    basePath: '/',
-    frameworks: ['jasmine'],
-    plugins: [
-        'karma-jasmine',
-        'karma-coverage',
-        'karma-mocha-reporter',
-        'karma-phantomjs-launcher',
-        'karma-chrome-launcher',
-        'karma-safari-launcher',
-        'karma-junit-reporter'
-    ],
-    reporters: [
-        'mocha',
-        'junit',
-        'coverage'
-    ],
-    junitReporter: {
-        outputDir: __dirname + '/../' + config.tests_dir + '/junit',
-        suite: ''
-    },
-    port: 9018,
-    runnerPort: 9100,
-    preprocessors: {},
-    urlRoot: '/',
-    colors: true,
-    autoWatch: true,
-    singleRun: true,
-    browsers: ['PhantomJS'],
-    coverageReporter: {
-        type: 'html'
-    }
-};
+  basePath: '/',
+  frameworks: ['jasmine'],
+  plugins: [
+    'karma-jasmine',
+    'karma-coverage',
+    'karma-mocha-reporter',
+    'karma-phantomjs-launcher',
+    'karma-chrome-launcher',
+    'karma-safari-launcher',
+    'karma-junit-reporter'
+  ],
+  reporters: [
+    'mocha',
+    'junit',
+    'coverage'
+  ],
+  junitReporter: {
+    outputDir: __dirname + '/../' + config.tests_dir + '/junit',
+    suite: ''
+  },
+  port: 9018,
+  runnerPort: 9100,
+  preprocessors: {},
+  urlRoot: '/',
+  colors: true,
+  autoWatch: true,
+  singleRun: true,
+  browsers: ['PhantomJS'],
+  coverageReporter: {
+    type: 'html',
+    dir: __dirname + '/../' + config.tests_dir + '/test-coverage'
+  }
+}
 
-var vendorFiles = [];
+karmaSettings.preprocessors[__dirname + '/../' + config.compile_dir + '/' + config.variables.testing_dir + '**/*.js'] = ['coverage']
 
-config.vendor_files.js.forEach(function(val) {
-    vendorFiles.push(__dirname + '/../' + val);
-});
+var vendorFiles = []
 
-config.test_files.js.forEach(function(val) {
-    vendorFiles.push(__dirname + '/../' + val);
-});
+config.vendor_files.js.forEach(function (val) {
+  vendorFiles.push(__dirname + '/../' + val)
+})
+
+config.test_files.js.forEach(function (val) {
+  vendorFiles.push(__dirname + '/../' + val)
+})
 
 
-gulp.task('build-tests', function() {
-    return gulp.src('src/' + config.variables.testing_dir + '**/*.spec.js')
-        .pipe(plumber(function(error) {
-            if (arguments.production) {
-                throw new Error(error.message);
-            }
-        }))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest(config.tests_dir + '/build'));
-});
+gulp.task('build-tests', function () {
+  return gulp.src('src/' + config.variables.testing_dir + '**/*.spec.js')
+    .pipe(plumber(function (error) {
+      if (arguments.production) {
+        throw new Error(error.message)
+      }
+    }))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(config.tests_dir + '/build'))
+})
 
-gulp.task('run-karma', function(done) {
-    var sources = [
-        __dirname + '/../' + config.compile_dir + '/assets/gitcommit.js',
-        __dirname + '/../' + config.compile_dir + '/app/**/*.js',
-        __dirname + '/../' + config.compile_dir + '/common/**/*.js',
-        __dirname + '/../' + config.compile_dir + '/' + config.tpl_name
-    ];
-    sources.push(__dirname + '/../' + config.tests_dir + "/build/**/*.js");
-    var files = vendorFiles.concat(sources);
-    karmaSettings.preprocessors[__dirname + '/../' + config.compile_dir + '/' + config.variables.testing_dir + '**/*.js'] = ['coverage'];
-    karmaSettings.coverageReporter['dir'] = __dirname + '/../' + config.tests_dir + '/test-coverage';
-    return gulp.src(files).pipe(karma(karmaSettings));
-});
+gulp.task('run-karma', function (done) {
 
-afterTestsBuild = function(done) {
-    if (GLOBAL.coffeeOK) {
-        runSequence('run-karma', done);
-    } else {
-        GLOBAL.coffeeOK = true;
-        done();
-    }
-};
+  var sources = [
+    __dirname + '/../' + config.compile_dir + '/assets/gitcommit.js',
+    __dirname + '/../' + config.compile_dir + '/app/**/*.js',
+    __dirname + '/../' + config.compile_dir + '/common/**/*.js',
+    __dirname + '/../' + config.compile_dir + '/' + config.tpl_name,
+    __dirname + '/../' + config.tests_dir + '/build/**/*.js'
+  ]
 
-gulp.task('run-tests', function(done) {
-    return runSequence('clean-tests', 'build-tests', function() {
-        runSequence('run-karma', done);
-    });
-});
 
-gulp.task('run-tests-watch', function(done) {
-    return runSequence('build-tests', function() {
-        runSequence('run-karma', done);
-    });
-});
+  var files = vendorFiles.concat(sources)
+
+  return gulp.src(files).pipe(karma(karmaSettings))
+})
+
+afterTestsBuild = function (done) {
+  if (GLOBAL.coffeeOK) {
+    runSequence('run-karma', done)
+  } else {
+    GLOBAL.coffeeOK = true
+    done()
+  }
+}
+
+gulp.task('run-tests', function (done) {
+  runSequence('clean-tests', 'build-tests', function () {
+    runSequence('run-karma', done)
+  })
+})
+
+gulp.task('run-tests-watch', function (done) {
+  runSequence('build-tests', function () {
+    runSequence('run-karma', done)
+  })
+})
 
