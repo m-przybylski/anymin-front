4d2fddae35d973e28255dc5964990bc4bfef575f
Jakub Pęksa <jakub.peksa@itelo.pl>
2016-01-05 12:11:08 +0100

    adding accountsessionApi resource

---
+++ src/common/api/AccountsSessionApi.js
@@ -0,0 +1,14 @@
+angular.module('profitelo.api.accountsSession', [
+  'ngResource',
+  'profitelo.services.commonSettings'
+])
+
+.factory('AccountsSessionApi', AccountsSessionApi)
+
+function AccountsSessionApi($resource, CommonSettingsService) {
+  return $resource(CommonSettingsService.get('apiUrl') + '/accounts/:telcoLogin/session', {telcoLogin: '@telcoLogin'}, {
+    'get': {method: 'GET', isArray: false},
+    'query': {method: 'GET', isArray: false},
+    // /accounts/{accountId}/status
+  })
+}
