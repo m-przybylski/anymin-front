15763f46b2eb8259b28214984118009804afbce3
Maciej Sypień <maciejsypien@gmail.com>
2016-01-08 17:46:48 +0100

    update create-new-service view

--- src/app/wizards/create-new-service/create-new-service.js
+++ src/app/wizards/create-new-service/create-new-service.js
@@ -1,22 +1,55 @@
-function createNewServiceController($interval) {
+function createNewServiceController($interval, _) {
   var vm = this
 
   vm.progress = 0
 
-  vm.currentService = 1
+  vm.currentActiveService = 1
 
   vm.directivesMapping = [
     {
       id:             'addServiceName',
       order:          1,
-      methodOnSave:   'saveServiceName'
+      methodOnSave:   'saveServiceName',
+      percentage:     10
     },
     {
       id:             'addServiceIndustry',
       order:          2,
-      methodOnSave:   'saveServiceIndystry'
+      methodOnSave:   'saveServiceIndystry',
+      percentage:     10
     }
   ]
+
+  vm.emitContextSaveAndGoNext = () => {
+    if (vm.currentActiveService > vm.directivesMapping.lenght) {
+      vm.currentActiveService += 1
+    } else {
+      // finish creating service
+    }
+  }
+
+  vm.goBack = () => {
+    if (vm.currentActiveService > 1) {
+      vm.currentActiveService -= 1
+    }
+  }
+
+
+  vm.calculateFillPercentage = () => {
+    var result = 0
+    var i = 0
+    if (i<0) {
+      while (i<vm.currentActiveService) {
+        console.log('time:', i)
+        var index = _.findIndex(vm.directivesMapping, function(item) {
+          return item.order === i
+        })
+        result += vm.directivesMapping[index].percentage
+        i++
+      }
+    }
+    return result
+  }
 
   return vm
 
@@ -24,7 +57,11 @@ function createNewServiceController($interval) {
 
 angular.module('profitelo.controller.wizards.create-new-service', [
   'ui.router',
-  'profitelo.directives.wizards.pro-create-new-service-name'
+  'lodash',
+
+  'profitelo.directives.pro-progress-bar',
+  'profitelo.directives.wizards.pro-create-new-service-name',
+  'profitelo.directives.wizards.pro-create-new-service-description'
 ])
 
 .config(function($stateProvider) {
