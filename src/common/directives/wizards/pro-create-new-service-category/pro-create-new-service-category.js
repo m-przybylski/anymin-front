d8c66c7fc78fa61480200335d9898cc684790582
Jakub Pęksa <jakub.peksa@itelo.pl>
2016-01-13 15:12:42 +0100

    new-service-category directive

--- src/common/directives/wizards/pro-create-new-service-category/pro-create-new-service-category.js
+++ src/common/directives/wizards/pro-create-new-service-category/pro-create-new-service-category.js
@@ -1,34 +1,30 @@
-function proCreateNewServiceCategoryController($scope, $filter, $http, toastr, _) {
-  var vm = this
+function proCreateNewServiceCategory($http) {
 
-  var _account      = $scope.account
-  var _userProfiles = $scope.userProfiles
+  function linkFunction(scope, elem, attrs) {
+    scope.categories = ['Podatki', 'Prawo finansowe', 'Księgowość', 'Prawo podatkowe', 'Dokumenty', 'Pomoc biurowa', 'Biuro rachunkowe', 'Inwestycje']
 
-  vm.save = () => {
-
+    scope.selectedCategory = ""
+    scope.selectCategory = (category) =>{
+      if(scope.selectedCategory==category) {
+        scope.selectedCategory = ""
+      }else{
+        scope.selectedCategory = category
+      }
+    }
   }
-  vm.categories = ['Podatki', 'Prawo finansowe', 'Księgowość', 'Prawo podatkowe', 'Dokumenty', 'Pomoc biurowa', 'Biuro rachunkowe', 'Inwestycje']
-  return vm
-}
-
-angular.module('profitelo.directives.wizards.pro-create-new-service-category', [
-  'ngAnimate',
-  'toastr',  // some parts depends on ngAnimate
-  'lodash',
-
-  // internal scripts
-  'profitelo.api.profiles'
-])
-
-.directive('proCreateNewServiceCategory', function() {
   return {
     replace:        true,
     templateUrl:    'directives/wizards/pro-create-new-service-category/pro-create-new-service-category.tpl.html',
-    controller:     proCreateNewServiceCategoryController,
-    controllerAs:   'vm',
     scope: {
       userProfile:  '='
-    }
+    },
+    link: linkFunction
   }
-})
+
+}
+
+angular.module('profitelo.directives.wizards.pro-create-new-service-category', [
+])
+
+.directive('proCreateNewServiceCategory', proCreateNewServiceCategory)
 
