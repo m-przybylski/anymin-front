eaa9ba773465228aae089c38ce81a134e25b3740
Jakub Pęksa <jakub.peksa@itelo.pl>
2016-01-13 14:15:44 +0100

    category wizard styles

--- src/common/directives/wizards/pro-create-new-service-category/pro-create-new-service-category.js
+++ src/common/directives/wizards/pro-create-new-service-category/pro-create-new-service-category.js
@@ -4,17 +4,11 @@ function proCreateNewServiceCategoryController($scope, $filter, $http, toastr, _
   var _account      = $scope.account
   var _userProfiles = $scope.userProfiles
 
-  vm.categories = ['Podatki', 'Prawo finansowe', 'Księgowość', 'Prawo podatkowe', 'Dokumenty', 'Pomoc biurowa', 'Biuro rachunkowe', 'Inwestycje']
   vm.save = () => {
 
   }
-
-
-
+  vm.categories = ['Podatki', 'Prawo finansowe', 'Księgowość', 'Prawo podatkowe', 'Dokumenty', 'Pomoc biurowa', 'Biuro rachunkowe', 'Inwestycje']
   return vm
-}
-function linkFunction() {
-
 }
 
 angular.module('profitelo.directives.wizards.pro-create-new-service-category', [
@@ -31,7 +25,6 @@ angular.module('profitelo.directives.wizards.pro-create-new-service-category', [
     replace:        true,
     templateUrl:    'directives/wizards/pro-create-new-service-category/pro-create-new-service-category.tpl.html',
     controller:     proCreateNewServiceCategoryController,
-    link:           linkFunction,
     controllerAs:   'vm',
     scope: {
       userProfile:  '='
