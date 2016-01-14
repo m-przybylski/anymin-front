4571c73f7c06a75ef74820f6f22737d1f7ab6a83
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-01-14 17:12:31 +0100

    add editing functionality to progress bar

--- src/common/directives/pro-progress-bar/pro-progress-bar.js
+++ src/common/directives/pro-progress-bar/pro-progress-bar.js
@@ -1,36 +1,44 @@
-function proProgressBarLink(scope) {
+function proProgressBar($rootScope) {
 
-  if (!scope.caption || scope.caption.length === 0) {
-    throw new Error('proProgressBar needs caption parameter to work.')
+  function proProgressBarLink(scope) {
+
+    if (!scope.caption || scope.caption.length === 0) {
+      throw new Error('proProgressBar needs caption parameter to work.')
+    }
+
+    if (!scope.progress) {
+      scope.progress = {
+        value: 0
+      }
+    } else {
+      scope.progress = {
+        value: scope.progress
+      }
+    }
+
+    scope.cancelEditing = () => {
+      $rootScope.$broadcast('cancelEditing')
+    }
+
+    scope.saveEditing = () => {
+      $rootScope.$broadcast('saveEditing')
+    }
+
   }
 
-
-  if (!scope.progress) {
-    scope.progress = {
-      value: 0
-    }
-  } else {
-    scope.progress = {
-      value: scope.progress
+  return {
+    restrict: 'EA',
+    replace: true,
+    link: proProgressBarLink,
+    templateUrl: 'directives/pro-progress-bar/pro-progress-bar.tpl.html',
+    scope: {
+      progress: '=?',
+      caption: '@',
+      next: '=',
+      queue: '='
     }
   }
-
 }
 
-angular.module('profitelo.directives.pro-progress-bar', [
-  'pascalprecht.translate'
-])
+angular.module('profitelo.directives.pro-progress-bar', ['pascalprecht.translate']).directive('proProgressBar', proProgressBar)
 
-.directive('proProgressBar', function() {
-  return {
-    restrict:     'EA',
-    replace:      true,
-    link: proProgressBarLink,
-    templateUrl:  'directives/pro-progress-bar/pro-progress-bar.tpl.html',
-    scope: {
-      progress: '=?proProgressBar',
-      caption: '@',
-      next: '='
-    }
-  }
-})
