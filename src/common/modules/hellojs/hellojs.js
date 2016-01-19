6558fef18af82fe1bb5a9ba83adf8aec85e265de
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-01-19 12:01:34 +0100

    fix js lint errors

--- src/common/modules/hellojs/hellojs.js
+++ src/common/modules/hellojs/hellojs.js
@@ -6,11 +6,17 @@ angular.module('hellojs', [
 
 function HellojsService($q) {
 
-  hello.init({
-    'facebook': '661427837225126',
-    'google': '996661759407-pf3q2igbo7e1oq5o9v6p1pjjnrvd7qe3.apps.googleusercontent.com',
-    'linkedin': '75q0l9bhc33fyn'
-  }, {redirect_uri: '/', scope: 'email'})
+  hello.init(
+    {
+      'facebook': '661427837225126',
+      'google': '996661759407-pf3q2igbo7e1oq5o9v6p1pjjnrvd7qe3.apps.googleusercontent.com',
+      'linkedin': '75q0l9bhc33fyn'
+    },
+    {
+      'redirect_uri': '/',
+      'scope': 'email'
+    }
+  )
 
 
   let _authResponseIsValid
@@ -35,8 +41,8 @@ function HellojsService($q) {
           vars: {
             socialNetwork: socialNetworkName
           }
-        };
-        return GlobalNotificationsService.add(_msg);
+        }
+        return GlobalNotificationsService.add(_msg)
       } else {
         return console.log('Singed out from ' + socialNetworkName)
       }
@@ -49,7 +55,7 @@ function HellojsService($q) {
           vars: {
             errorMessage: e.error.message
           }
-        };
+        }
         return GlobalNotificationsService.add(_msg)
       } else {
         return console.log('Signed out error:' + e.error.message)
@@ -59,7 +65,7 @@ function HellojsService($q) {
 
 
   let _login
-  _login = function (socialNetworkName) {
+  _login = function(socialNetworkName) {
     var authResponse, deferred
     authResponse = hello.getAuthResponse(socialNetworkName)
     if (_authResponseIsValid(authResponse)) {
@@ -67,22 +73,21 @@ function HellojsService($q) {
       deferred.resolve(authResponse)
       return deferred.promise
     } else {
-      return hello(socialNetworkName).login().then(function (auth) {
+      return hello(socialNetworkName).login().then(function(auth) {
         if (_authResponseIsValid(auth.authResponse)) {
           return auth
         } else {
           console.log('authResponseIsValid error. auth object:', auth)
           return false
         }
-      }, function (e) {
+      }, function(e) {
         console.log('hello(socialNetworkName).login() error:', e)
         return false
       })
     }
   }
-  let api
 
-  api = {
+  let api = {
     authResponseIsValid: _authResponseIsValid,
     logout: _logout,
     login: _login,
@@ -90,18 +95,18 @@ function HellojsService($q) {
       var authResponse
       authResponse = hello.getAuthResponse(socialNetworkName)
       if (_authResponseIsValid(authResponse)) {
-        console.log("resend auth response", authResponse)
+        console.log('resend auth response', authResponse)
         return _me(socialNetworkName, false)
       } else {
         return hello(socialNetworkName).login().then(function(auth) {
           if (_authResponseIsValid(auth.authResponse)) {
             return _me(socialNetworkName, false)
           } else {
-            console.log("Error in: hello(socialNetworkName).login()")
+            console.log('Error in: hello(socialNetworkName).login()')
             return false
           }
         }, function(e) {
-          console.log("Error", e)
+          console.log('Error', e)
         })
       }
     }
