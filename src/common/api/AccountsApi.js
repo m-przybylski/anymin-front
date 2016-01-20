1bdabf21bc7880a697e6f2828edbf586ecf979b9
Maciej Sypień <maciejsypien@gmail.com>
2016-01-20 17:16:30 +0100

    megre AccountStatusApi with AccountApi module

--- src/common/api/AccountsApi.js
+++ src/common/api/AccountsApi.js
@@ -1,14 +1,22 @@
-angular.module('profitelo.api.accounts', [
-  'ngResource',
-  'profitelo.services.commonSettings'
-])
-
-.factory('AccountsApi', AccountsApi)
-
-function AccountsApi($resource, CommonSettingsService) {
+function accountsApi($resource, CommonSettingsService) {
   return $resource(CommonSettingsService.get('apiUrl') + '/accounts/:telcoLogin', {telcoLogin: '@telcoLogin'}, {
     'get': {method: 'GET', isArray: true},
     'query': {method: 'GET', isArray: false}
   // /accounts/{accountId}/status
   })
 }
+
+function accountsStatusApi($resource, CommonSettingsService) {
+  return $resource(CommonSettingsService.get('apiUrl') + '/accounts/:telcoLogin/status', {telcoLogin: '@telcoLogin'}, {
+    'get': {method: 'GET', isArray: false},
+    'query': {method: 'GET', isArray: false}
+    // /accounts/{accountId}/status
+  })
+}
+
+angular.module('profitelo.api.accounts', [
+  'ngResource',
+  'profitelo.services.commonSettings'
+])
+.factory('AccountsApi', accountsApi)
+.factory('AccountsStatusApi', accountsStatusApi)
