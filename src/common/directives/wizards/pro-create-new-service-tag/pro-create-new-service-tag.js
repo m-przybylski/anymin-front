b65e6c30dfbeab625bf470b41422e78a092babc8
Jakub Pęksa <jakub.peksa@itelo.pl>
2016-02-01 13:21:02 +0100

    adding tags to create service wizard

--- src/common/directives/wizards/pro-create-new-service-tag/pro-create-new-service-tag.js
+++ src/common/directives/wizards/pro-create-new-service-tag/pro-create-new-service-tag.js
@@ -1,21 +1,66 @@
-function proCreateNewServiceTag($http) {
+function proCreateNewServiceTag($http, wizardSectionControlService, _) {
 
-  function linkFunction(scope, elem, attrs) {
+  function linkFunction(scope, element, attrs) {
     scope.tags = ['VAT', 'Prawo finansowe', 'Księgowość', 'Prawo podatkowe', 'PIT', 'CIT', 'Biuro rachunkowe', 'Inwestycje']
-    scope.tagMap = new Object()
-    angular.forEach(scope.tags, (value, key) =>{
-      scope.tagMap[value] = false
-    })
-
-    scope.selectTag = (tag) =>{
-      scope.tagMap[tag] = !scope.tagMap[tag]
+    scope.model = {
+      tagMap: new Object()
     }
+    angular.forEach(scope.tags, (value, key) =>{
+      scope.model.tagMap[value] = false
+    })
+    scope.selectTag = (tag) =>{
+      scope.model.tagMap[tag] = !scope.model.tagMap[tag]
+    }
+
+    scope.saveSection = () => {
+      console.log('save section: ', parseInt(scope.order, 10))
+    }
+
+    let _isValid = () => {
+      let _valid = false
+      angular.forEach(scope.tags, (value, key) =>{
+        if (scope.model.tagMap[value] === true) {
+          return _valid = true
+        }
+      })
+      return _valid
+    }
+
+    let _getModel = () => {
+      return scope.model
+    }
+
+    let _setModel = (model) => {
+      scope.model = angular.copy(model)
+    }
+
+    scope.config = {
+      order:    parseInt(scope.order, 10),
+      model:    scope.model,
+      element:  element,
+      queue:    scope.queue,
+      save:     scope.saveSection,
+      isValid:  _isValid,
+      getModel: _getModel,
+      setModel: _setModel,
+      toggles: {
+        show:         false,
+        past:         false,
+        beingEdited:  false
+      }
+    }
+    console.log(scope.config)
+    wizardSectionControlService(scope.config)
+
   }
   return {
     replace:        true,
     templateUrl:    'directives/wizards/pro-create-new-service-tag/pro-create-new-service-tag.tpl.html',
     scope: {
-      userProfile:  '='
+      userProfile:  '=',
+      queue:    '=',
+      order:    '@',
+      service:  '='
     },
     link: linkFunction
   }
