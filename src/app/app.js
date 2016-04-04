2813317ed838d02d5023838f6090d549fb68f57e
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-04-04 13:36:27 +0200

    adobe typekit integration && login page abstract state

--- src/app/app.js
+++ src/app/app.js
@@ -50,7 +50,44 @@ function configFunction($urlRouterProvider, $httpProvider, $stateProvider, $reso
     url: '',
     abstract: true,
     controller: 'AppController',
-    templateUrl: 'templates/app.tpl.html'
+    templateUrl: 'templates/app.tpl.html',
+    resolve: {
+      typeKit: ($q) => {
+
+        var deferred = $q.defer()
+
+        let config = {
+            kitId: 'gxk2sou',
+            scriptTimeout: 3000,
+            async: true,
+            active: function() {
+              deferred.resolve()
+            }
+          },
+          h = document.documentElement, t = setTimeout(() => {
+            h.className = h.className.replace(/\bwf-loading\b/g, '') + ' wf-inactive'
+          }, config.scriptTimeout), tk = document.createElement('script'), f = false, s = document.getElementsByTagName('script')[0], a
+        h.className += ' wf-loading'
+        tk.src = 'https://use.typekit.net/' + config.kitId + '.js'
+        tk.async = true
+        tk.onload = tk.onreadystatechange = function() {
+          a = this.readyState
+          if (f || a && a !== 'complete' && a !== 'loaded') {
+            return
+          }
+          f = true
+          clearTimeout(t)
+          try {
+            Typekit.load(config)
+          } catch (e) {
+            deferred.reject(e)
+          }
+        }
+        s.parentNode.insertBefore(tk, s)
+
+        return deferred.promise
+      }
+    }
   })
   $urlRouterProvider
     .when('', '/')
