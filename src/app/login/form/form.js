80b325b154d3f5d53f4e82014550860a13e28de8
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-04-07 11:23:46 +0200

    merge

--- src/app/login/form/form.js
+++ src/app/login/form/form.js
@@ -1,14 +1,38 @@
 (function() {
 
-  function LoginFormController() {
+  function LoginFormController($q, $timeout) {
     var vm = this
 
     vm.current = 1
     vm.groupsHistory = []
 
-    vm.next = () => {
+    vm.isPending = false
+    
+    // User input variables
+    vm.phoneNumber = {
+      prefix: '',
+      number: ''
+    }
+    
+
+    vm.getPhoneNumberStatus = () => {
+      if (!vm.isPending) {
+        vm.isPending = true
+        $timeout(function() {
+          vm.isPending = false
+          vm.next(2)
+        }, Math.floor((Math.random() * 20) + 1) * 100)
+      }
+    }
+
+    vm.next = (stepNumber = -1) => {
       vm.groupsHistory.push(vm.current)
-      vm.current++
+      if (stepNumber >= 0) {
+        vm.current = stepNumber
+      } else {
+        vm.current++
+      }
+
     }
 
     vm.prev = () => {
