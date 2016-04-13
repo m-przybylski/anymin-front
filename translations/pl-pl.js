649013f2a52c1b146cc03894a4d972e2e2b8241f
Pawel Budzyk <pawel.budzyk@itelo.pl>
2016-04-13 09:50:08 +0200

    Add Create-Profile files

--- translations/pl-pl.js
+++ translations/pl-pl.js
@@ -11,6 +11,11 @@
         		'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER_UPLOAD':'Ukończono',
         		'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO':'lub kliknij by wgrać pliki',
         		'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO_UPLOAD':'plik {{ file }} z {{ files }}',
+        		'DASHBOARD.CREATE_PROFILE.CREATE_PROFILE_TITLE':'Tworzenie konta usługodawcy',
+        		'DASHBOARD.CREATE_PROFILE.NEXT_STEP':'Kolejny Etap',
+        		'DASHBOARD.CREATE_PROFILE.STEP1_PROGRESS_BAR_TITLE':'Etap 1 - Tworzenie profilu firmy',
+        		'DASHBOARD.MENU.CLIENT':'Klient',
+        		'DASHBOARD.MENU.EXPERT':'Ekspert',
         		'LOGIN.DOWNLOAD_FREE_APP':'Pobierz bezpłatną aplikację na telefon:',
         		'LOGIN.ENTER_NUMBER':'wpisz swój numer',
         		'LOGIN.ENTER_NUMBER_PLACEHOLDER':'np. 599-546-342',
@@ -24,7 +29,10 @@
         		'LOGIN.PREFIX_PLACEHOLDER':'+48',
         		'LOGIN.SERVICE_PASSWORD':'Podaj hasło do usługi',
         		'LOGIN.TRUST_EXPERTS':'Zaufaj ekspertom i rozwiąż swoje problemy',
-        		'LOGIN.VERIFICATION_ATTEMPTS_EXCEEDED':'Wpisałeś błędnie kod zbyt wiele razy. Poczekaj jeszcze {{time}} aby móc spróbować pownownie.'
+        		'LOGIN.VERIFICATION_ATTEMPTS_EXCEEDED':'Wpisałeś błędnie kod zbyt wiele razy. Poczekaj jeszcze {{time}} aby móc spróbować pownownie.',
+        		'REGISTER.CHANGE_PASSWORD':'Zmień',
+        		'REGISTER.SERVICE_CODE':'Wpisz kod weryfikacyjny z smsa',
+        		'REGISTER.SERVICE_CODE_PLACEHOLDER':'np. 476239'
         })
     }])
 })(angular);
