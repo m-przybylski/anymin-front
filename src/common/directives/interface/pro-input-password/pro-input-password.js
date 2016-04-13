e2cfaf8f6cec0f6a93f0073793fe551b94e87044
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-04-13 14:00:00 +0200

    improve pro-input-password and remove jqurey like code

--- src/common/directives/interface/pro-input-password/pro-input-password.js
+++ src/common/directives/interface/pro-input-password/pro-input-password.js
@@ -1,44 +1,51 @@
 function proInputPassword() {
 
   function linkFunction(scope, element, attr) {
-    let _inputGroup = $(element)
-    let _input = _inputGroup.find('input')
+
+
+    scope.required = false
     scope.focus = false
     scope.onOut = false
-    scope.showPassword = false
-    scope.focusInput = function() {
+    scope.inputType = 'password'
+
+    let _input = $(element).find('input')
+
+    if ('required' in attr.$attr) {
+      scope.required = true
+    }
+
+    scope.focusInput = function () {
       _input.focus()
     }
-    scope.onFocus = function() {
+    scope.onFocus = function () {
       scope.focus = true
       scope.onOut = false
     }
-    scope.onFocusOut = function() {
+    scope.onFocusOut = function () {
       scope.focus = false
       scope.onOut = true
     }
-    scope.passwordHandler = function() {
-      if (_input.attr('type') === 'password') {
-        _input.attr('type', 'text')
-        scope.showPassword = true
-      }else {
-        _input.attr('type', 'password')
-        scope.showPassword = false
+    scope.passwordHandler = function () {
+      if (scope.inputType === 'password') {
+        scope.inputType = 'text'
+      } else {
+        scope.inputType = 'password'
       }
     }
   }
 
   return {
-    templateUrl:  'directives/interface/pro-input-password/pro-input-password.tpl.html',
-    restrict:     'E',
-    replace:      true,
+    templateUrl: 'directives/interface/pro-input-password/pro-input-password.tpl.html',
+    restrict: 'E',
+    replace: true,
     link: linkFunction,
     scope: {
       proModel: '=',
       placeholder: '@',
       defaultValue: '@',
       label: '@',
-      onChange: '=?'
+      onChange: '=?',
+      name: '@'
     }
 
   }
@@ -47,4 +54,4 @@ function proInputPassword() {
 
 
 angular.module('profitelo.directives.interface.pro-input-password', [])
-.directive('proInputPassword', proInputPassword)
+  .directive('proInputPassword', proInputPassword)
