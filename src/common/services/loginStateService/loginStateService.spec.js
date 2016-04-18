93631f60ada773ca4f0c961f4393332e39e17249
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-04-18 13:56:23 +0200

    loginStateService tests

---
+++ src/common/services/loginStateService/loginStateService.spec.js
@@ -0,0 +1,56 @@
+describe('Unit testing: profitelo.services.login-state >', function() {
+  describe('for loginStateService service >', function() {
+
+    let loginStateService  = null
+
+    beforeEach(function() {
+      module('profitelo.services.login-state')
+
+      inject(($injector) => {
+        loginStateService = $injector.get('loginStateService')
+      })
+
+    })
+
+    it('should have a dummy test', function() {
+      expect(true).toBeTruthy()
+    })
+
+    it('should contain empty account object', function() {
+
+      let _account = {
+        phoneNumber: {
+          prefix: null,
+          number: null
+        },
+        password: ''
+      }
+
+      expect(loginStateService.getAccountObject()).toEqual(_account)
+
+    })
+
+
+    it('should contain change default account object', function() {
+
+      let _account = {
+        phoneNumber: {
+          prefix: '+45',
+          number: '2345676543'
+        },
+        password: ''
+      }
+
+      loginStateService.setAccountObject(_account)
+
+      expect(loginStateService.getAccountObject()).toEqual(_account)
+
+    })
+
+
+
+  })
+})
+
+
+
