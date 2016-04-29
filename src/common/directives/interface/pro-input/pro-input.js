b85cc2603328354bece59a0017fc014bbd64ffde
mikolaj.sikorski <mikolaj.sikorski@itelo.pl>
2016-04-29 11:39:25 +0200

    Fix regex, change colors, fix styles

--- src/common/directives/interface/pro-input/pro-input.js
+++ src/common/directives/interface/pro-input/pro-input.js
@@ -23,7 +23,8 @@ function proInput() {
     if ('onlyDigits' in attr.$attr) {
       element.bind('keypress', function(e) {
         let code = e.keyCode || e.which
-        if (code === 101 || code === 32) {
+        console.log("")
+        if (code === 101 || code === 32 || code === 46 || code === 45 || code === 44) {
           e.preventDefault()
         }
       })
