4e0cd8b015e18227be3c98b5b483180c4d12945e
Pawel Budzyk <pawel.budzyk@itelo.pl>
2016-05-09 13:52:57 +0200

    Refactor code directive

--- src/common/directives/service-provider/pro-bottom-summary-row/pro-bottom-summary-row.js
+++ src/common/directives/service-provider/pro-bottom-summary-row/pro-bottom-summary-row.js
@@ -1,26 +1,13 @@
 (function() {
   function proBottomSummaryRow($q) {
-
-    function linkFunction(scope, element, attrs) {
-      scope.model = {
-        width: ''
-      }
-    }
-
-
     return {
       replace: true,
       restrict: 'E',
       templateUrl: 'directives/service-provider/pro-bottom-summary-row/pro-bottom-summary-row.tpl.html',
       scope: {
-        queue:    '=',
-        order:    '=?',
         proModel: '=',
         width: '=',
-        trTitle: '@',
-        trDesc: '@'
       },
-      link: linkFunction
     }
   }
 
