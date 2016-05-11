ae1f9381846a000b05ef60ad99441245624b683e
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-05-11 15:03:50 +0200

    profile image upload, proUploader fixes

--- src/common/directives/interface/pro-uploader/pro-uploader.js
+++ src/common/directives/interface/pro-uploader/pro-uploader.js
@@ -1,44 +1,56 @@
-function proUploader($timeout, $interval, $q, FilesApi, Upload) {
+(function() {
 
-  function  linkFunction(scope, element, attr) {
-    let _file = 0
-    let _files = 0
-    let immediateInterval
-    scope.progress = 0
-    scope.fadeText = false
-    scope.header = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER'
-    scope.info = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO'
-    scope.upload = false
-    scope.hideArrow = false
+  function proUploader($timeout, $interval, $q, FilesApi, Upload) {
 
-    scope.$watch('proModel', ()=> {
-      _uploadFiles()
-    })
-
-    if ('type' in attr.$attr) {
-      scope.ngfPattern = attr.type
-      scope.accept = attr.type
-    }
-
-    let _calculatePercentage = function(loaded, total) {
-      return parseInt((100.0 * loaded / total), 10)
-    }
-
-
-    _uploadFiles = function() {
-      let files = scope.proModel
+    function  linkFunction(scope, element, attr) {
+      
       let _file = 0
-      var tokenPromisses = []
-      if (files && files.length) {
-        for (var i = 0; i < scope.proModel.length; i++) {
-          if (!scope.proModel[i].$error) {
-            tokenPromisses.push(FilesApi.tokenPath().$promise)
+      let _files = 0
+      let immediateInterval
+      scope.progress = 0
+      scope.fadeText = false
+      scope.header = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER'
+      scope.info = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO'
+      scope.upload = false
+      scope.hideArrow = false
+
+      scope.filesBaseModel = []
+
+      console.log(scope.filesUploaded)
+
+      scope.$watch(() => {
+        return scope.filesBaseModel
+      }, () => {
+
+        _uploadFiles(scope.filesBaseModel)
+      })
+
+      if ('type' in attr.$attr) {
+        scope.ngfPattern = attr.type
+        scope.accept = attr.type
+      }
+
+      let _calculatePercentage = function(loaded, total) {
+        return parseInt((100.0 * loaded / total), 10)
+      }
+
+
+      _uploadFiles = function(files) {
+
+        _file = 0
+
+        var tokenPromises = []
+
+        for (var i = 0; i < files.length; i++) {
+          if (!files[i].$error) {
+            tokenPromises.push(FilesApi.tokenPath().$promise)
           }
         }
-        $q.all(tokenPromisses).then( (tokenPromissesResponse) =>{
+
+        $q.all(tokenPromises).then( (tokenPromissesResponse) =>{
           scope.animate()
-          for (var k = 0; k < scope.proModel.length; k++ ) {
-            _files = scope.proModel.length
+          _files = files.length
+          for (var k = 0; k < files.length; k++ ) {
             Upload.upload({
               url:  tokenPromissesResponse[k].uploadUrl,
               data: {
@@ -46,11 +58,12 @@ function proUploader($timeout, $interval, $q, FilesApi, Upload) {
               }
             }).then(
               function(res) {
-                scope.filesUploaded.push(scope.proModel[_file])
+                console.log(scope.filesUploaded)
+                scope.filesUploaded.push(files[_file])
                 _file++
               },
               function(res) {
-              // TODO walidacje na odpowiedzi z serwera
+                // TODO walidacje na odpowiedzi z serwera
               },
               function(res) {
                 scope.progress = _calculatePercentage(res.loaded, res.total)
@@ -59,71 +72,71 @@ function proUploader($timeout, $interval, $q, FilesApi, Upload) {
           }
         }, function(tokenPromissesError) {
         })
+
       }
-    }
-    let _endImmediateLoading = () => {
-      scope.progress = 0
-      scope.fadeText = true
-      $timeout(()=>{
-        scope.header = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER'
-        scope.info = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO'
-      }, 200)
-      scope.hideLoader = true
-      scope.upload = false
-      scope.hideArrow = false
-      scope.showArrow = true
-
-    }
-    let _startImmediateLoading = () =>
-      immediateInterval = $interval(() => {
-        if (scope.progress >= 100) {
-          $interval.cancel(immediateInterval)
-          _endImmediateLoading()
-        }
-      }, 100)
-
-    scope.animate = function() {
-      scope.showArrow = false
-      scope.hideArrow = true
-      scope.hideLoader = false
-      scope.fadeText = false
-      $timeout(()=>{
+      let _endImmediateLoading = () => {
+        scope.progress = 0
         scope.fadeText = true
-        $timeout(()=> {
-          scope.upload = true
-          scope.header = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER_UPLOAD'
-          scope.info = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO_UPLOAD'
-          _startImmediateLoading()
-          scope.translationInfo = {
-            file: _file,
-            files: _files
+        $timeout(()=>{
+          scope.header = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER'
+          scope.info = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO'
+        }, 200)
+        scope.hideLoader = true
+        scope.upload = false
+        scope.hideArrow = false
+        scope.showArrow = true
+
+      }
+      let _startImmediateLoading = () =>
+        immediateInterval = $interval(() => {
+          if (scope.progress >= 100) {
+            $interval.cancel(immediateInterval)
+            _endImmediateLoading()
           }
-          $timeout(()=>{
-            scope.fadeText = false
+        }, 100)
+
+      scope.animate = function() {
+        scope.showArrow = false
+        scope.hideArrow = true
+        scope.hideLoader = false
+        scope.fadeText = false
+        $timeout(()=>{
+          scope.fadeText = true
+          $timeout(()=> {
+            scope.upload = true
+            scope.header = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.HEADER_UPLOAD'
+            scope.info = 'COMMON.DIRECTIVES.INTERFACE.UPLOADER.INFO_UPLOAD'
+            _startImmediateLoading()
+            scope.translationInfo = {
+              file: _file,
+              files: _files
+            }
+            $timeout(()=>{
+              scope.fadeText = false
+            }, 200)
           }, 200)
         }, 200)
-      }, 200)
 
+      }
+    }
+    return {
+      templateUrl: 'directives/interface/pro-uploader/pro-uploader.tpl.html',
+      restrict: 'E',
+      link: linkFunction,
+      scope: {
+        defaultValue: '@',
+        accept: '@',
+        ngfPattern: '@',
+        filesUploaded: '=',
+        maxSize: '@'
+
+      }
     }
   }
-  return {
-    templateUrl: 'directives/interface/pro-uploader/pro-uploader.tpl.html',
-    restrict: 'E',
-    replace: true,
-    link: linkFunction,
-    scope: {
-      proModel: '=',
-      defaultValue: '@',
-      accept: '@',
-      ngfPattern: '@',
-      filesUploaded: '=?',
-      maxSize: '@'
 
-    }
-  }
-}
-
-angular.module('profitelo.directives.interface.pro-uploader', [
-  'ngFileUpload'
-])
+  angular.module('profitelo.directives.interface.pro-uploader', [
+    'ngFileUpload'
+  ])
   .directive('proUploader', proUploader)
+
+}())
---
+++ src/common/directives/service-provider/pro-service-provider-avatar/pro-service-provider-avatar.js
@@ -0,0 +1,79 @@
+(function() {
+  function proServiceProviderAvatar($q, $timeout) {
+
+    function linkFunction(scope, element, attrs) {
+
+      scope.required = false
+      scope.badName = false
+
+      scope.model = {
+        avatar: []
+      }
+
+      let _isValid = () => {
+        let _isValidDeferred = $q.defer()
+
+        if (angular.isDefined(scope.model.name) && scope.model.name.length > 0) {
+          _isValidDeferred.resolve()
+        } else {
+          _isValidDeferred.reject()
+        }
+
+        return _isValidDeferred.promise
+      }
+
+
+
+      let _displayErrorMessage = () => {
+        scope.badName = true
+        $timeout(() => {
+          scope.badName = false
+        }, 1000)
+      }
+
+
+      if ('required' in attrs) {
+        scope.required = true
+      }
+
+      scope.saveSection = () => {
+        _isValid().then(() => {
+
+          scope.proModel.name = scope.model.name
+          scope.proceed()
+
+        }, () => {
+          _displayErrorMessage()
+        })
+      }
+      
+
+    }
+
+
+    return {
+      replace: true,
+      restrict: 'E',
+      templateUrl: 'directives/service-provider/pro-service-provider-avatar/pro-service-provider-avatar.tpl.html',
+      scope: {
+        queue: '=',
+        order: '=?',
+        proModel: '=',
+        trTitle: '@',
+        trDesc: '@',
+        placeholder: '@'
+      },
+      link: linkFunction,
+      controller: 'ServiceProviderStepController',
+      controllerAs: 'vm'
+    }
+  }
+
+  angular.module('profitelo.directives.service-provider.pro-service-provider-avatar', [
+    'lodash',
+    'pascalprecht.translate',
+    'profitelo.common.controller.service-provider.service-provider-step-controller',
+    'profitelo.directives.interface.pro-uploader'
+  ])
+  .directive('proServiceProviderAvatar', proServiceProviderAvatar)
+}())
