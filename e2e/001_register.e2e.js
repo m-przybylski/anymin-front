1ac341ba43928714ce3a0b6ea827cb0f266ac558
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-05-25 09:58:36 +0200

    login e2e tests

--- e2e/001_register.e2e.js
+++ e2e/001_register.e2e.js
@@ -10,4 +10,40 @@ describe('Sign-up page', function () {
     var text = browser.findElement(by.css('.top-div h5'))
     expect(text.getText()).toEqual('Zaufaj ekspertom i rozwiąż swoje problemy')
   })
+
+
+  it('should redirect to register page for unregistered number', function () {
+    var phoneNumber = browser.findElement(by.model('ngModel'))
+    var form = element(by.name('phoneNumberForm'))
+    phoneNumber.sendKeys('123456789')
+    form.submit()
+    var text = browser.findElement(by.css('.pro-input-form .input-group label'))
+
+    expect(text.getText()).toEqual('WPISZ KOD WERYFIKACYJNY Z SMSA')
+
+  })
+
+  it('should login to a known account', function () {
+
+    browser.get(testUrl)
+
+    var phoneNumber = browser.findElement(by.model('ngModel'))
+    var form = element(by.name('phoneNumberForm'))
+    phoneNumber.sendKeys('000000000')
+    form.submit()
+
+    var password = browser.findElement(by.model('ngModel'))
+    var form = element(by.name('passwordForm'))
+
+    password.sendKeys('admin123')
+    form.submit()
+    
+    expect(element(by.css('.dashboard-menu')).isPresent()).toBeTruthy();
+
+
+  })
+
+
+
+
 })
