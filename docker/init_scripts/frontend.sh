01b592326d3f9f46491fedad591c7a77b270ddd5
Mariusz Bełtowski <mariusz.beltowski@itelo.pl>
2016-06-16 12:43:18 +0000

    Update frontend.sh, add md5 flag

--- docker/init_scripts/frontend.sh
+++ docker/init_scripts/frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /profitelo-frontend && gulp serve --turbo-translations --no-tests --no-live-reload
+cd /profitelo-frontend && gulp serve --turbo-translations --no-tests --no-live-reload --md5
