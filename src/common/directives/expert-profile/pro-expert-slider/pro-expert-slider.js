67f832d8d53ce4efec3ca2b9262e39e973d51590
Pawel Budzyk <pawel.budzyk@itelo.pl>
2016-06-20 15:19:52 +0200

    Create document sliders

--- src/common/directives/expert-profile/pro-expert-slider/pro-expert-slider.js
+++ src/common/directives/expert-profile/pro-expert-slider/pro-expert-slider.js
@@ -1,5 +1,30 @@
 (function() {
   function proExpertSlider() {
+    function linkFunction(scope) {
+
+      var controlsClick = 0
+
+      scope.prevSlide = () => {
+        if (controlsClick > 0) {
+          let width = $('.slider-slides').width()
+          width = width/3
+          $('.slider-slides').animate({'left': '+=295px'}, 'slow')
+          controlsClick = controlsClick - 1
+        }
+      }
+
+      scope.nextSlide = () => {
+        let itemCount = $('.slides').length
+        let currentSlides = itemCount-3
+
+        if (currentSlides > controlsClick) {
+          let width = $('.slider-slides').width()
+          width = width/3
+          $('.slider-slides').animate({'left': '-=295px'}, 'slow')
+          controlsClick = controlsClick + 1
+        }
+      }
+    }
 
     return {
       templateUrl: 'directives/expert-profile/pro-expert-slider/pro-expert-slider.tpl.html',
@@ -7,7 +32,8 @@
       replace: true,
       scope: {
         sliders: '=?'
-      }
+      },
+      link: linkFunction
     }
   }
 
