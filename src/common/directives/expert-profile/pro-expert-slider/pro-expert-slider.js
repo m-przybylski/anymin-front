8cfb4310a8fac0316209bbecfa64fa4b23ce2232
Pawel Budzyk <pawel.budzyk@itelo.pl>
2016-06-22 09:43:48 +0200

    Change href to ui-sref in start page

--- src/common/directives/expert-profile/pro-expert-slider/pro-expert-slider.js
+++ src/common/directives/expert-profile/pro-expert-slider/pro-expert-slider.js
@@ -6,8 +6,6 @@
 
       scope.prevSlide = () => {
         if (controlsClick > 0) {
-          let width = $('.slider-slides').width()
-          width = width/3
           $('.slider-slides').animate({'left': '+=295px'}, 'slow')
           controlsClick = controlsClick - 1
         }
@@ -16,10 +14,7 @@
       scope.nextSlide = () => {
         let itemCount = $('.slides').length
         let currentSlides = itemCount-3
-
         if (currentSlides > controlsClick) {
-          let width = $('.slider-slides').width()
-          width = width/3
           $('.slider-slides').animate({'left': '-=295px'}, 'slow')
           controlsClick = controlsClick + 1
         }
