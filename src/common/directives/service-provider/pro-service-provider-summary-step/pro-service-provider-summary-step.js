4734fbf2e2e3c916653b5dfde2a7c2508e3ea831
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-06-27 12:26:20 +0200

    swap html to tags directive

--- src/common/directives/service-provider/pro-service-provider-summary-step/pro-service-provider-summary-step.js
+++ src/common/directives/service-provider/pro-service-provider-summary-step/pro-service-provider-summary-step.js
@@ -1,11 +1,28 @@
 (function() {
-  function proServiceProviderSummaryStep($q) {
+  function proServiceProviderSummaryStep() {
 
+    function preCompileFunction(scope) {
+      for (let consultation of scope.consultations) {
+        if (consultation.invitations.length > 0) {
+          consultation.invitationTags = consultation.invitations.map((invitation) => {
+            return invitation.email
+          })
+        }
+      }
+    }
+
+    function compileFunction() {
+      return {
+        pre: preCompileFunction
+      }
+    }
+    
     return {
       replace: true,
       restrict: 'E',
       templateUrl: 'directives/service-provider/pro-service-provider-summary-step/pro-service-provider-summary-step.tpl.html',
       transclude: true,
+      compile: compileFunction,
       scope: {
         consultations: '=',
         editAction: '=',
@@ -20,7 +37,8 @@
     'profitelo.common.controller.service-provider.pro-service-provider-summary-controller',
     'lodash',
     'pascalprecht.translate',
-    'profitelo.common.controller.service-provider.service-provider-step-controller'
+    'profitelo.common.controller.service-provider.service-provider-step-controller',
+    'profitelo.components.pro-summary-tag'
   ])
   .directive('proServiceProviderSummaryStep', proServiceProviderSummaryStep)
 }())
