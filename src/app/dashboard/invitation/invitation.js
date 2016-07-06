8c108211595efa69920802f4552b25d50feb921a
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2016-07-06 13:38:17 +0200

    resolve invitation info from api

--- src/app/dashboard/invitation/invitation.js
+++ src/app/dashboard/invitation/invitation.js
@@ -1,33 +1,9 @@
 (function() {
 
-  function InvitationController() {
 
-    this.profile = {
-      name: 'Jakaś firma',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.',
-      logo: '/assets/images/John_Doe.jpg',
-      invitations: [
-        {
-          icon: '',
-          industry: 'Biznes > Metody zarządzania firmą',
-          tags: [
-            'Biznesplan',
-            'Małe firmy',
-            'Srednie firmy'
-          ]
-        },
-        {
-          icon: '',
-          industry: 'Prawo > Prawo Rodzinne',
-          tags: [
-            'Rozwody',
-            'Alimenty',
-            'Spadki i darowizny'
-          ]
-        }
-      ]
+  function InvitationController(invitations) {
 
-    }
+    this.invitations = invitations
     
     return this
   }
@@ -41,6 +17,28 @@
       data: {
         access: UserRolesProvider.getAccessLevel('user'),
         pageTitle: 'PAGE_TITLE.LOGIN.ACCOUNT'
+      },
+      resolve: {
+        invitations: ($q, $state, ProfileApi, User) => {
+          /* istanbul ignore next */
+          let _deferred = $q.defer()
+          /* istanbul ignore next */
+          User.getStatus().then(() => {
+            ProfileApi.getProfilesInvitations().$promise.then((response) => {
+              _deferred.resolve(response[0])
+            }, () => {
+              _deferred.resolve(null)
+            })
+          }, (error) => {
+            $state.go('app.dashboard')
+            proTopAlertService.error({
+              message: 'error',
+              timeout: 4
+            })
+          })
+          /* istanbul ignore next */
+          return _deferred.promise
+        }
       }
     })
   }
--- src/common/directives/dashboard/invitation/pro-invitation-acceptance-box/pro-invitation-acceptance-box.js
+++ src/common/directives/dashboard/invitation/pro-invitation-acceptance-box/pro-invitation-acceptance-box.js
@@ -1,20 +1,64 @@
 (function() {
-  function proInvitationAcceptanceBox() {
 
-    return {
+    function controllerFunction(EmploymentApi) {
+
+      let _isPending = false
+
+      this.accept = (employmentId) => {
+
+        if (!_isPending) {
+          _isPending = true
+          EmploymentApi.postEmploymentsAccept({
+            employmentId: 1
+          }).$promise.then((res) => {
+            _isPending = false
+            console.log(res)
+          }, () => {
+            _isPending = false
+          })
+        }
+
+      }
+
+      this.reject = (employmentId) => {
+
+        if (!_isPending) {
+          _isPending = true
+          EmploymentApi.postEmploymentsReject({
+            employmentId: 1
+          }).$promise.then((res) => {
+            _isPending = false
+            console.log(res)
+          }, () => {
+            _isPending = false
+          })
+        }
+
+      }
+
+      return this
+
+    }
+
+  controllerFunction.$inject = ['EmploymentApi']
+
+
+  let proInvitationAcceptanceBox = {
       templateUrl: 'directives/dashboard/invitation/pro-invitation-acceptance-box/pro-invitation-acceptance-box.tpl.html',
       restrict: 'E',
       replace: true,
-      scope: {
-        invitation: '='
-      }
+      bindings: {
+        invitation: '<'
+      },
+      controller: controllerFunction
     }
-  }
+
 
   angular.module('profitelo.directives.dashboard.invitation.pro-invitation-acceptance-box', [
     'profitelo.components.pro-summary-tag',
-    'profitelo.directives.interface.pro-input'
+    'profitelo.directives.interface.pro-input',
+    'profitelo.swaggerResources'
   ])
-    .directive('proInvitationAcceptanceBox', proInvitationAcceptanceBox)
+    .component('proInvitationAcceptanceBox', proInvitationAcceptanceBox)
 
 }())
