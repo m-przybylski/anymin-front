8f5939027e61dd21790cd45869f5e00c049994af
Artur Wędzicha <artur.wedzicha@itelo.pl>
2016-07-29 10:22:05 +0200

    tagMore

--- src/common/components/pro-summary-tag/pro-summary-tag.js
+++ src/common/components/pro-summary-tag/pro-summary-tag.js
@@ -1,4 +1,45 @@
 (function() {
+  /* @ngInject */
+  function showMoreTagsController($element, $timeout)  {
+
+    let elementsMap = []
+    let containerWidth = $element.width()
+    this.showTags = false
+
+    function findTags(selector = 'li') {
+      return $.map($($element).find(selector), (li)=>{
+        return li
+      })
+    }
+
+    this.showMoreTags = () => {
+      elementsMap = findTags('li.hidden')
+
+      for (var i=0; i<elementsMap.length; i++) {
+        elementsMap[i].classList.remove("hidden")
+      }
+      this.showTags = !this.showTags
+    }
+
+    $timeout(()=>{
+      elementsMap = findTags()
+
+      let sum = 0
+      for (var i=0; i<elementsMap.length; i++) {
+        sum += elementsMap[i].clientWidth
+
+        if (sum > containerWidth) {
+          elementsMap[i-1].classList.add("hidden")
+        }
+      }
+
+      if (sum > (containerWidth)) {
+        this.showTags = !this.showTags
+      }
+    })
+
+    return this
+  }
 
   let proSummaryTag = {
     transclude: true,
@@ -6,7 +47,9 @@
     bindings: {
       tags: '<',
       title: '@'
-    }
+    },
+    controller: showMoreTagsController,
+    controllerAs: 'vm'
   }
 
   angular.module('profitelo.components.pro-summary-tag', [
