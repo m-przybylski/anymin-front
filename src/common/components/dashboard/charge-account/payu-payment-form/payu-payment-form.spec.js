784fb6fd7c47179879b5fe017dcc4f572475980b
mikolaj.sikorski <mikolaj.sikorski@itelo.pl>
2016-09-19 10:26:55 +0200

    add validations on Dirty and add unit tests

--- src/common/components/dashboard/charge-account/payu-payment-form/payu-payment-form.spec.js
+++ src/common/components/dashboard/charge-account/payu-payment-form/payu-payment-form.spec.js
@@ -7,8 +7,13 @@ describe('Unit testing: profitelo.components.dashboard.charge-account.payu-payme
     let compile
     let componentController
     let component
+    let state
     let bindings
-    let validHTML = '<payu-payment-form></payu-payment-form>'
+    let PaymentsApi
+    let _httpBackend
+    let PaymentApiDef
+    let resourcesExpectations
+    let validHTML = '<payu-payment-form ></payu-payment-form>'
 
     beforeEach(module(function($provide) {
       $provide.value('apiUrl', url)
@@ -24,29 +29,59 @@ describe('Unit testing: profitelo.components.dashboard.charge-account.payu-payme
 
     beforeEach(() => {
       module('templates-module')
+      module('profitelo.swaggerResources.definitions')
       module('profitelo.components.dashboard.charge-account.payu-payment-form')
-
-      inject(($rootScope, $compile, _$componentController_) => {
+      module('ui.router')
+      
+      inject(($rootScope, $compile, _$componentController_, $httpBackend,_PaymentsApi_, _$state_, _PaymentApiDef_) => {
         componentController = _$componentController_
         rootScope = $rootScope.$new()
         compile = $compile
+        state = _$state_
+        PaymentsApi = _PaymentsApi_
+        _httpBackend = $httpBackend
+        PaymentApiDef = _PaymentApiDef_
       })
-      bindings = {
 
+      resourcesExpectations = {
+        PaymentsApi: {
+          postPayUOrder: _httpBackend.when(PaymentApiDef.postPayUOrder.method, PaymentApiDef.postPayUOrder.url)
+        }
+      }
+      
+      bindings = {
+        amountMethodModal: {
+          firstName: 'DUMB_NAME',
+          lastName: 'DUMB_LASTNAME',
+          email: 'DUMB_EMAIL',
+          payMethodValue: '1',
+          amountModel: {
+            amount: '2122'
+          },
+          paymentSystemModel: {
+            id: 1
+          }
+        },
+        validAction: () => {
+          return true
+        }
       }
 
       component = componentController('payuPaymentForm',null, bindings)
-
+      
+      expect(component.amountMethodModal).toBeDefined();
     })
 
     it('should have a dummy test', inject(() => {
       expect(true).toBeTruthy()
     }))
 
-    it('should compile the component', () => {
-      let el = create(validHTML)
-      expect(el.html()).toBeDefined(true)
-    })
+    it('should call isValid', inject(() => {
+      let sendPayment = jasmine.createSpy('sendPayment');
+      component.sendPayment()
+      expect(sendPayment).toBeCalled()
+    }))
+
   })
 })
 
