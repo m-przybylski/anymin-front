0846000c1524c9d0d27407368b1b60fbcb96da4a
Mariusz Bełtowski <mariusz.beltowski@itelo.pl>
2016-11-16 07:56:53 +0100

    Add proTopWaitingLoaderService tests

--- src/common/services/proTopWaitingLoaderService/proTopWaitingLoaderService.spec.js
+++ src/common/services/proTopWaitingLoaderService/proTopWaitingLoaderService.spec.js
@@ -14,5 +14,24 @@ describe('Unit testing: profitelo.services.pro-top-waiting-loader-service >', ()
     it('should have a dummy test', () => {
       expect(true).toBeTruthy()
     })
+
+    it('should run immediate function and callback', inject(($timeout, $interval) => {
+
+      const cbs = {
+        progressCallback: _ => _
+      }
+
+      spyOn($interval, 'cancel').and.callThrough()
+      spyOn(cbs, 'progressCallback')
+
+      proTopWaitingLoaderService.bindProgress(cbs.progressCallback)
+      proTopWaitingLoaderService.immediate()
+      $timeout.flush()
+      $interval.flush(10000)
+
+      expect($interval.cancel).toHaveBeenCalled()
+      expect(cbs.progressCallback).toHaveBeenCalled()
+    }))
+
   })
 })
