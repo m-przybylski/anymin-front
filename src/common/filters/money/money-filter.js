6f72623540e9eb39a89b3c2ddf8af80d36108051
Mariusz Bełtowski <mariusz.beltowski@itelo.pl>
2016-11-21 15:58:10 +0100

    Refactor money-filter

--- src/common/filters/money/money-filter.js
+++ src/common/filters/money/money-filter.js
@@ -1,22 +1,27 @@
 (function() {
+
+  const handleAmount = (_amount) => {
+    const amount = parseInt(_amount, 10)
+    const major = (amount) ? parseInt(amount / 100, 10) : 0
+    const minor = (amount) ? amount % 100 : 0
+    const minorFirst = (minor) ? minor % 10 : 0
+    const minorSecond = (minor) ? parseInt(minor / 10, 10) : 0
+    return major + '.' + minorSecond + minorFirst
+  }
+
+  const handleMoney = (_money) => {
+    return handleAmount(_money.amount) + ' ' + _money.currency
+  }
+
   function filter($log) {
     return function(money) {
-
-      let result = ''
 
       if (!money || typeof money !== 'object' || !money.hasOwnProperty('amount') || !money.hasOwnProperty('currency')) {
         $log.error('Money filter argument is not a money object')
         return ''
       }
 
-      const amount = parseInt(money.amount, 10)
-      const major = (amount) ? parseInt(amount / 100, 10) : 0
-      const minor = (amount) ? amount % 100 : 0
-      const minorFirst = (minor) ? minor % 10 : 0
-      const minorSecond = (minor) ? parseInt(minor / 10, 10) : 0
-      result += major + '.' + minorSecond + minorFirst + ' ' + money.currency
-
-      return result
+      return handleMoney(money)
     }
   }
 
