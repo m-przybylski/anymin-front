b2139d4f7331ac5dc943e6360bc0f12e4033a8ce
Artur Wędzicha <arturwedzicha@pc40.home>
2017-02-03 10:58:31 +0100

    fix unit tests

--- src/app/dashboard/settings/general/general.spec.ts
+++ src/app/dashboard/settings/general/general.spec.ts
@@ -1,36 +1,21 @@
-describe('Unit tests: DashboardClientActivitiesController >', () => {
+describe('Unit tests: DashboardSettingsGeneralController >', () => {
   describe('Testing Controller: DashboardClientActivitiesController', () => {
 
-    let DashboardClientActivitiesController
-
-    const clientActivities = {
-      activities: [],
-      balance: {},
-      expertServiceTuples: [],
-      activityTypes: []
-    }
-
-    const clientActivitiesService = {
-      onQueryParamsChange: _ => _,
-      onActivitiesResults: _ => _,
-      clearQueryParam: _ => _
-    }
+    let DashboardSettingsGeneralController
 
     beforeEach(() => {
-    angular.mock.module('profitelo.controller.dashboard.client.activities')
+    angular.mock.module('profitelo.controller.dashboard.settings.general')
 
       inject(($rootScope, $controller, _$state_) => {
-        DashboardClientActivitiesController = $controller('DashboardClientActivitiesController', {
+        DashboardSettingsGeneralController = $controller('DashboardSettingsGeneralController', {
           $state: _$state_,
-          $scope: $rootScope.$new(),
-          clientActivities: clientActivities,
-          clientActivitiesService: clientActivitiesService
+          $scope: $rootScope.$new()
         })
       })
     })
 
     it('should exists', () => {
-      expect(!!DashboardClientActivitiesController).toBe(true)
+      expect(!!DashboardSettingsGeneralController).toBe(true)
     })
 
   })
--- src/app/dashboard/settings/security/security.spec.ts
+++ src/app/dashboard/settings/security/security.spec.ts
@@ -3,28 +3,13 @@ describe('Unit tests: DashboardSettingsSecurityController >', () => {
 
     let DashboardSettingsSecurityController
 
-    const clientActivities = {
-      activities: [],
-      balance: {},
-      expertServiceTuples: [],
-      activityTypes: []
-    }
-
-    const clientActivitiesService = {
-      onQueryParamsChange: _ => _,
-      onActivitiesResults: _ => _,
-      clearQueryParam: _ => _
-    }
-
     beforeEach(() => {
     angular.mock.module('profitelo.controller.dashboard.settings.security')
 
       inject(($rootScope, $controller, _$state_) => {
         DashboardSettingsSecurityController = $controller('DashboardSettingsSecurityController', {
           $state: _$state_,
-          $scope: $rootScope.$new(),
-          clientActivities: clientActivities,
-          clientActivitiesService: clientActivitiesService
+          $scope: $rootScope.$new()
         })
       })
     })
