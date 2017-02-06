dc13e6469aa669a4a94b9649e0bfd0ac1532cfef
Artur Wędzicha <arturwedzicha@MacBook-Pro-Artur.local>
2017-02-06 15:32:12 +0100

    added phone settings style

--- src/common/components/dashboard/settings/modals/general/phone-settings/phone-settings.ts
+++ src/common/components/dashboard/settings/modals/general/phone-settings/phone-settings.ts
@@ -1,36 +1,38 @@
 namespace profitelo.components.dashboard.settings.modals.general.phoneSettings {
 
-  export interface IGeneralPhoneSettingsControllerParentScope extends ng.IScope {}
-
-  export interface IGeneralPhoneSettingsControllerScope extends ng.IScope {
+  interface IgeneralPhoneSettingsControllerScope extends ng.IScope {
+    isHidden: boolean
+    onModalClose: Function
     isNavbar: boolean
     isFullscreen: boolean
-    isAvatarVisableToExpert: boolean
-    onModalClose: Function
-    addPhoto: Function
-    imageSource: string
-    $parent: IGeneralPhoneSettingsControllerParentScope
+    verifyCode: Function
   }
 
   export class GeneralPhoneSettingsController implements ng.IController {
 
     /* @ngInject */
-    constructor($scope: IGeneralPhoneSettingsControllerScope,
-                $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance) {
+    constructor(private $scope: IgeneralPhoneSettingsControllerScope,
+                private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance) {
 
+      $scope.isHidden = true
       $scope.isNavbar = true
       $scope.isFullscreen = true
 
+      $scope.verifyCode = () => {
+        $scope.isHidden = false
+      }
+
       $scope.onModalClose = () =>
         $uibModalInstance.dismiss('cancel')
-    }
 
+    }
   }
 
   angular.module('profitelo.components.dashboard.settings.modals.general.phone-settings', [
     'ui.bootstrap',
     'profitelo.directives.interface.pro-input',
-    'profitelo.directives.interface.scrollable'
+    'profitelo.directives.interface.scrollable',
+    'profitelo.components.interface.pin-verification'
   ])
   .controller('generalPhoneSettingsController', GeneralPhoneSettingsController)
 
---
+++ src/common/components/interface/pin-verification/pin-verification.ts
@@ -0,0 +1,23 @@
+namespace profitelo.components.communicator {
+
+  class PinVerificationController implements ng.IController {
+
+    /* @ngInject */
+    constructor() {
+
+    }
+
+  }
+
+  class PinVerificationComponent implements ng.IComponentOptions {
+
+    controller: ng.Injectable<ng.IControllerConstructor> = PinVerificationController
+    templateUrl: string = 'components/interface/pin-verification/pin-verification.tpl.html'
+  }
+
+  angular.module('profitelo.components.interface.pin-verification', [
+    'pascalprecht.translate',
+    'profitelo.directives.interface.pro-input'
+  ])
+  .component('pinVerification', new PinVerificationComponent())
+}
