7848a0426a23a584d474befdec59be130f55af23
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-03-06 11:23:13 +0100

    add braintree transaction

--- src/common/components/dashboard/charge-account/payment-method/card/card.ts
+++ src/common/components/dashboard/charge-account/payment-method/card/card.ts
@@ -1,31 +1,69 @@
 namespace profitelo.components.dashboard.chargeAccount.paymentMethod.cardPaymentForm {
 
-  export class CardPaymentFormComponentController implements ng.IController {
-    isInvoice : boolean
-    onBraintreeFormLoad: boolean
-    onLoad: any
+  import MoneyDto = profitelo.api.MoneyDto
+  import PaymentLink = profitelo.api.PaymentLink
 
+  export interface ITransaction {
+    amount: MoneyDto
+    paymentCountryId: string
+    paymentOption: MoneyDto
+    paymentSystemId: string
+  }
+
+  // TODO TO BE FIXED after charge-account typing fix.
+  export interface ICardPaymentFormComponentBindings {
+    paymentsLinks: Array<PaymentLink>
+    amountMethodModal: any
+    paymentCountryId: string
+  }
+
+  export class CardPaymentFormComponentController implements ng.IController, ICardPaymentFormComponentBindings {
+    isInvoice: boolean
+    onBraintreeFormLoad: boolean
+    paymentCountryId: string
+    transaction: ITransaction
+    paymentsLinks: Array<PaymentLink>
+    amountMethodModal: any
+
+    $onInit = () => {
+      this.transaction =  {
+        amount: this.amountMethodModal.amountModel.cashAmount,
+        paymentCountryId: this.paymentCountryId,
+        paymentOption: this.amountMethodModal.amountModel.amount,
+        paymentSystemId: this.amountMethodModal.paymentSystemModel.id
+      }
+    }
     /* @ngInject */
-    constructor() {
+    constructor(private $state: ng.ui.IStateService) {
       this.isInvoice = false
       this.onBraintreeFormLoad = false
+    }
 
-      this.onLoad = () => {
-        this.onBraintreeFormLoad = true
-      }
+
+    public onLoad = (): void => {
+      this.onBraintreeFormLoad = true
+    }
+
+    public onSucceed = (): void => {
+      this.$state.go('app.dashboard.client.activities')
     }
   }
 
   class CardPaymentFormComponent implements ng.IComponentOptions {
     controllerAs: '$ctrl'
     controller: ng.Injectable<ng.IControllerConstructor> = CardPaymentFormComponentController
-    templateUrl: string =  'components/dashboard/charge-account/payment-method/card/card.tpl.html'
+    templateUrl: string = 'components/dashboard/charge-account/payment-method/card/card.tpl.html'
     replace: true
+    bindings: {[boundProperty: string]: string} = {
+      paymentsLinks: '<',
+      amountMethodModal: '<',
+      paymentCountryId: '<'
+    }
   }
-
 
   angular.module('profitelo.components.dashboard.charge-account.payment-method.card', [
     'pascalprecht.translate',
+    'ui.router',
     'profitelo.components.interface.preloader',
     'profitelo.components.braintree-form',
   ])
