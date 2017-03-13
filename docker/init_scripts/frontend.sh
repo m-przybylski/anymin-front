3caef6432764e58e4799f53f4ae673c3c0f08e60
Paweł Dzięcioł <pawel.dzieciol@itelo.pl>
2017-03-13 16:54:21 +0100

    Added npm run tr and npm run build for deploys. Changed nginx config to serve files from dist directory.

--- docker/init_scripts/frontend.sh
+++ docker/init_scripts/frontend.sh
@@ -3,4 +3,7 @@
 cd /profitelo-frontend
 npm install
 #gulp serve --turbo-translations --no-tests --no-live-reload --md5 2>&1 | tee -a /var/log/gulp.log
-npm run serve 2>&1 | tee -a /var/log/gulp.log
+#npm run serve 2>&1 | tee -a /var/log/gulp.log
+npm run tr
+npm run build-full
+sleep infinity
