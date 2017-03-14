9ffa5d0a41ce26ca48c4b41ee6749cf02043e241
Paweł Dzięcioł <pawel.dzieciol@itelo.pl>
2017-03-14 12:15:43 +0000

    Update frontend.sh

--- docker/init_scripts/frontend.sh
+++ docker/init_scripts/frontend.sh
@@ -4,6 +4,5 @@ cd /profitelo-frontend
 npm install
 #gulp serve --turbo-translations --no-tests --no-live-reload --md5 2>&1 | tee -a /var/log/gulp.log
 #npm run serve 2>&1 | tee -a /var/log/gulp.log
-npm run tr
 npm run ci-deploy
 sleep infinity
