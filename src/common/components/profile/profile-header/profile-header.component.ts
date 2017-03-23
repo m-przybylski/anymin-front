269b3b5ada2ef1e67468d1465f00b5cd418b02ef
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-03-23 10:32:55 +0100

    add single consultation and header logic

--- src/common/components/profile/profile-header/profile-header.component.ts
+++ src/common/components/profile/profile-header/profile-header.component.ts
@@ -1,9 +1,10 @@
 import {ProfileHeaderComponentController} from './profile-header.controller'
 export class ProfileHeaderComponent implements ng.IComponentOptions {
-  controllerAs: '$ctrl'
   controller: ng.Injectable<ng.IControllerConstructor> = ProfileHeaderComponentController
   template = require('./profile-header.pug')()
-  replace: true
   bindings: {[boundProperty: string]: string} = {
+    profileDetails: '<',
+    isFavourite: '<',
+    onLike: '<'
   }
 }
--- src/common/components/profile/profile-header/profile-header.controller.ts
+++ src/common/components/profile/profile-header/profile-header.controller.ts
@@ -1,10 +1,14 @@
 import {IProfileHeaderComponentBindings} from './profile-header'
+import {GetExpertDetails} from 'profitelo-api-ng/model/models'
+
 export class ProfileHeaderComponentController implements IProfileHeaderComponentBindings {
 
+  profileDetails?: GetExpertDetails
+  isFavourite: boolean
+  onLike: () => void
 
   /* @ngInject */
   constructor() {
-
 
   }
 }
