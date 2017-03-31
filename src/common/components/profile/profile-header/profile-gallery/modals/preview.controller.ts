177fcb493c510c0e4ccd3b57b00ea46f53e77398
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-03-31 08:56:06 +0200

    fix previews

--- src/common/components/profile/profile-header/profile-gallery/modals/preview.controller.ts
+++ src/common/components/profile/profile-header/profile-gallery/modals/preview.controller.ts
@@ -1,22 +1,11 @@
-import {UrlService} from '../../../../../services/url/url.service'
-
 export interface IGalleryPreviewControllerScope extends ng.IScope {
-  token: string
+  preview: string
 }
 
 export class GalleryPreviewController implements ng.IController {
 
   /* @ngInject */
-  constructor(private urlService: UrlService) {
-
-  }
-
-  public createUrl = (imageToken: string) => {
-    if (imageToken) {
-      return this.urlService.resolveFileUrl(imageToken)
-    } else {
-      return 'no-image'
-    }
+  constructor() {
   }
 
 }
