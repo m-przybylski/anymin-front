2d17a4e4f5733f0a69ff2e9a77c094a0822652f8
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-06-06 14:34:40 +0200

    add wizard uploader progress

--- src/common/components/wizard/wizard-uploader/wizard-uploader.controller.ts
+++ src/common/components/wizard/wizard-uploader/wizard-uploader.controller.ts
@@ -4,11 +4,18 @@ import {UploaderService} from '../../../services/uploader/uploader.service'
 import {FilesApi} from 'profitelo-api-ng/api/api'
 import {PostProcessOption, FileInfo} from 'profitelo-api-ng/model/models'
 import * as _ from 'lodash'
+
+export interface IDocumentFile {
+  file?: File,
+  fileInfo?: FileInfo,
+  fileUploadInfo?: any
+}
+
 export class WizardUploaderComponentController implements IWizardUploaderModuleComponentBindings {
 
   private uploader: UploaderService
-  public uploadedFiles: Array<FileInfo> = []
-  public tokenList: Array<string>
+  public documentFiles: IDocumentFile[] = []
+  public tokenList: string[]
 
   /* @ngInject */
   constructor(private $log: ng.ILogService, uploaderFactory: UploaderFactory, private FilesApi: FilesApi) {
@@ -18,43 +25,52 @@ export class WizardUploaderComponentController implements IWizardUploaderModuleC
   $onInit() {
     this.tokenList.forEach((token) => {
       this.FilesApi.fileInfoPath(token).then((response) => {
-        this.uploadedFiles.push(response)
+        const documentObject = {
+          fileInfo: response
+        }
+        this.documentFiles.push(documentObject)
       }, (error) => {
         throw new Error('Can not get File Info: ' + error)
       })
     })
   }
 
-  private onUploadProgess = (res: any) =>
-    this.$log.debug(res)
-
   private postProcessOptions: PostProcessOption = {
     croppingDetails: undefined
-  }
-
-  private onFilesUpload = (res: any) => {
-    this.uploadedFiles.push(res)
-    this.tokenList.push(res.token)
   }
 
   private onFileUploadError = (err: any) => {
     this.$log.error(err)
   }
 
-  public uploadFiles = (files: Array<File>) => {
+  public uploadFiles = (files: File[]) => {
     files.forEach((file) => {
-      this.uploader.uploadFile(file, this.postProcessOptions, this.onUploadProgess)
-      .then(this.onFilesUpload, this.onFileUploadError)
+      const currentFile: IDocumentFile = {
+        file: file
+      }
+      this.documentFiles.push(currentFile)
+      this.uploader.uploadFile(file, this.postProcessOptions, (res: any) => {
+        currentFile.fileUploadInfo = res
+      })
+      .then((res: any) => {
+        currentFile.fileInfo = res
+        this.tokenList.push(res.token)
+      }, this.onFileUploadError)
     })
   }
 
-  public removeFile = (file: FileInfo) => {
-    _.remove(this.uploadedFiles, (currentFile) => {
+  public removeFile = (file: IDocumentFile) => {
+    _.remove(this.documentFiles, (currentFile) => {
       return currentFile === file
     })
 
     _.remove(this.tokenList, (token) => {
-      return file.token === token
+      if (file.fileInfo) {
+        return file.fileInfo.token === token
+      } else {
+        this.$log.error('Can not find file')
+        return void 0
+      }
     })
 
   }
