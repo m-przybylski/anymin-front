18424ed958f52a0a5d8c3879573d9fb862fdd742
Artur Wędzicha <artur.wedzicha@itelo.pl>
2017-09-05 13:34:56 +0200

    added plus button to component and plus icon to avatar-crop

--- src/common/components/wizard/wizard-links/wizard-links.ts
+++ src/common/components/wizard/wizard-links/wizard-links.ts
@@ -2,6 +2,7 @@ import * as angular from 'angular'
 import {WizardLinksComponent} from './wizard-links.component'
 import '../../../directives/pro-social-icon-getter/pro-social-icon-getter'
 import ValidationAlertModule from '../../interface/alert/validation-alert/validation-alert'
+import './wizard-links.sass'
 
 export interface IWizardLinksComponentBindings extends ng.IController {
   selectedLinks: string[]
