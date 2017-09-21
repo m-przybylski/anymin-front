a40be1f8ce19d0918165ce5155e8206dda0451db
Janusz Chwastek <janusz.chwastek@itelo.pl>
2017-09-21 11:43:14 +0200

    feature - edit expert profile

--- src/common/components/dashboard/expert/manage-profile/modals/edit-expert-profile/edit-expert-profile.controller.ts
+++ src/common/components/dashboard/expert/manage-profile/modals/edit-expert-profile/edit-expert-profile.controller.ts
@@ -1,28 +1,117 @@
-import {IDropdownInputDictionary} from '../../../../../interface/input-dropdown-tag/input-dropdown-tag.controller'
+import {ProfileApi} from 'profitelo-api-ng/api/api';
+import {TopAlertService} from '../../../../../../services/top-alert/top-alert.service'
+import {GetOrganizationDetails, GetExpertDetails, OrganizationDetailsUpdate, ExpertDetailsUpdate}
+  from 'profitelo-api-ng/model/models';
+
 export interface IEditExpertProfileScope extends ng.IScope {
+  profile: GetOrganizationDetails | GetExpertDetails
+  onModalCloseCallback: () => void
 }
 
 export class EditExpertProfileController implements ng.IController {
+
+  public profileAvatarToken: string
+  public profileFilesTokens: string[] = []
+  public profileName: string
+  public profileDescription: string
+  public profileLinks: string[] = []
   public isLoading: boolean = false
   public isFullscreen: boolean = true
   public isNavbar: boolean = true
-  public inputText: string
-  public textareaDescription: string
-  public readonly inputMaxLength: number = 150
-  public tagsMocks: IDropdownInputDictionary
-  public companyPathModel = {
-    links: ''
-  }
-  public isAvatarUploaded: boolean = false
+  public isSubmitted: boolean = false
+  public readonly inputDescriptionMaxLength: number = 600
+  public readonly inputNameMaxLength: number = 150
 
-  public onModalClose = (): void =>
-    this.$uibModalInstance.dismiss('cancel')
+  private static readonly minValidExpertNameLength: number = 3
+  private static readonly minValidExpertDescriptionLength: number = 50
+  private isUploading: boolean = true
 
   /* @ngInject */
-  constructor(private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance) {
-    this.inputText = 'Jan Kowalski'
-    this.textareaDescription = 'Lorem Ipsum is simply. It is a long established fact that a reader will be ' +
-      'distracted by the readable content of a page when looking at its layout'
+  constructor(private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance,
+              private ProfileApi: ProfileApi,
+              private $log: ng.ILogService,
+              private topAlertService: TopAlertService,
+              private $filter: ng.IFilterService,
+              private $scope: IEditExpertProfileScope) {}
 
+  $onInit(): void {
+    if (this.isGetExpertDetails(this.$scope.profile)) {
+      this.profileAvatarToken = this.$scope.profile.avatar
+    } else {
+      this.profileAvatarToken = this.$scope.profile.logo
+    }
+      this.profileName = this.$scope.profile.name
+      this.profileDescription = this.$scope.profile.description
+      this.profileLinks = this.$scope.profile.links
+      this.$scope.profile.files.forEach((file) => {
+        this.profileFilesTokens.push(file.token)
+    })
   }
+
+  public saveChanges = (): void => {
+    if (this.isFormValid() && this.isGetExpertDetails(this.$scope.profile)) {
+      const updatedProfile = {
+        expertDetails: {
+          name: this.profileName,
+          avatar: this.profileAvatarToken,
+          description: this.profileDescription,
+          files: this.profileFilesTokens,
+          links: this.profileLinks
+        }
+      }
+      this.sendUpdatedProfile(updatedProfile)
+    } else if (this.isFormValid() && !this.isGetExpertDetails(this.$scope.profile)) {
+      const updatedProfile = {
+        organizationDetails: {
+          name: this.profileName,
+          logo: this.profileAvatarToken,
+          description: this.profileDescription,
+          files: this.profileFilesTokens,
+          links: this.profileLinks
+        }
+      }
+      this.sendUpdatedProfile(updatedProfile)
+    } else {
+      this.isSubmitted = true
+    }
+  }
+
+  public onModalClose = (): void => this.$uibModalInstance.dismiss('cancel')
+
+  public isAvatarValid = (): boolean => !!(this.profileAvatarToken && this.profileAvatarToken.length > 0)
+
+  public isNameValid = (): boolean => !!(this.profileName
+    && this.profileName.length >= EditExpertProfileController.minValidExpertNameLength)
+
+  public isDescriptionValid = (): boolean => !!(this.profileDescription
+    && this.profileDescription.length >= EditExpertProfileController.minValidExpertDescriptionLength)
+
+  private isFileUploadValid = (): boolean => this.isUploading
+
+  public onUploadingFile = (status: boolean): void => {
+    this.isUploading = status
+  }
+
+  public isFormValid = (): boolean =>
+    this.isAvatarValid()
+    && this.isNameValid()
+    && this.isDescriptionValid()
+    && this.isFileUploadValid()
+
+  private isGetExpertDetails = (profile: GetOrganizationDetails | GetExpertDetails): profile is GetExpertDetails =>
+    (<GetExpertDetails>profile).avatar !== undefined
+
+  private sendUpdatedProfile = (updatedProfile: OrganizationDetailsUpdate | ExpertDetailsUpdate): void => {
+    this.ProfileApi.patchProfileRoute(updatedProfile).then((_res) => {
+      this.$scope.onModalCloseCallback()
+      this.onModalClose()
+    }, (err) => {
+      this.$log.error(err)
+      this.topAlertService.warning({
+        message: this.$filter('translate')('DASHBOARD.EXPERT_ACCOUNT.MANAGE_PROFILE.MODAL.SAVE_ERROR_MESSAGE'),
+        timeout: 5
+      })
+    })
+  }
+
 }
--- src/common/components/dashboard/expert/manage-profile/modals/edit-expert-profile/edit-expert-profile.spec.ts
+++ src/common/components/dashboard/expert/manage-profile/modals/edit-expert-profile/edit-expert-profile.spec.ts
@@ -2,10 +2,12 @@ import * as angular from 'angular'
 import IRootScopeService = profitelo.services.rootScope.IRootScopeService
 import manageProfileEditProfileModule from './edit-expert-profile'
 import {EditExpertProfileController, IEditExpertProfileScope} from './edit-expert-profile.controller'
+import {ProfileApi} from 'profitelo-api-ng/api/api';
 describe('Testing Controller: editExpertProfileController', () => {
 
   let editExpertProfileController: EditExpertProfileController
   let scope: IEditExpertProfileScope
+  let ProfileApi: ProfileApi
   const uibModalInstance = {
     dismiss: (): void => {
 
@@ -21,16 +23,22 @@ describe('Testing Controller: editExpertProfileController', () => {
 
   beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
     $provide.value('apiUrl', 'awesomeURL')
+    $provide.value('topAlertService', {})
   }))
 
   beforeEach(() => {
-    inject(($rootScope: IRootScopeService, $controller: ng.IControllerService) => {
+    inject(($rootScope: IRootScopeService,
+            $controller: ng.IControllerService,
+            _ProfileApi_: ProfileApi) => {
+
+      ProfileApi = _ProfileApi_
 
       scope = <IEditExpertProfileScope>$rootScope.$new()
 
       editExpertProfileController = $controller(EditExpertProfileController, {
         '$scope': scope,
-        '$uibModalInstance': uibModalInstance
+        '$uibModalInstance': uibModalInstance,
+        'ProfileApi': ProfileApi
       })
     })
   })
@@ -44,5 +52,73 @@ describe('Testing Controller: editExpertProfileController', () => {
     editExpertProfileController.onModalClose()
     expect(uibModalInstance.dismiss).toHaveBeenCalledWith('cancel')
   })
+
+  it('should not save profile ', () => {
+    editExpertProfileController.profileName = 'a'
+    editExpertProfileController.saveChanges()
+    expect(editExpertProfileController.isSubmitted).toBe(true)
+  })
+
+  it('should save expert profile', inject(($q: ng.IQService) => {
+    spyOn(ProfileApi, 'patchProfileRoute').and.callFake(() => $q.resolve({}))
+    editExpertProfileController.profileName = 'SomeName'
+    editExpertProfileController.profileAvatarToken = 'someToken'
+    editExpertProfileController.profileDescription = 'someDescription someDescription someDescription someDescription'
+    scope.profile = {
+      name: 'name',
+      avatar: 'avatar',
+      description: 'someDescription someDescription someDescription someDescription',
+      files: [{
+        token: 'token',
+        previews: ['prev']
+      }],
+      links: ['link']
+    }
+    const updatedProfile = {
+      expertDetails: {
+        name: 'SomeName',
+        avatar: 'someToken',
+        description: 'someDescription someDescription someDescription someDescription',
+        files: [],
+        links: []
+      }
+    }
+    const status: boolean = true
+    editExpertProfileController.onUploadingFile(status)
+    editExpertProfileController.saveChanges()
+    expect(editExpertProfileController.isSubmitted).toBe(false)
+    expect(ProfileApi.patchProfileRoute).toHaveBeenCalledWith(updatedProfile)
+  }))
+
+  it('should save organization profile', inject(($q: ng.IQService) => {
+    spyOn(ProfileApi, 'patchProfileRoute').and.callFake(() => $q.resolve({}))
+    editExpertProfileController.profileName = 'SomeName'
+    editExpertProfileController.profileAvatarToken = 'someToken'
+    editExpertProfileController.profileDescription = 'someDescription someDescription someDescription someDescription'
+    scope.profile = {
+      name: 'name',
+      logo: 'avatar',
+      description: 'someDescription someDescription someDescription someDescription',
+      files: [{
+        token: 'token',
+        previews: ['prev']
+      }],
+      links: ['link']
+    }
+    const updatedProfile = {
+      organizationDetails: {
+        name: 'SomeName',
+        logo: 'someToken',
+        description: 'someDescription someDescription someDescription someDescription',
+        files: [],
+        links: []
+      }
+    }
+    const status: boolean = true
+    editExpertProfileController.onUploadingFile(status)
+    editExpertProfileController.saveChanges()
+    expect(editExpertProfileController.isSubmitted).toBe(false)
+    expect(ProfileApi.patchProfileRoute).toHaveBeenCalledWith(updatedProfile)
+  }))
 
 })
