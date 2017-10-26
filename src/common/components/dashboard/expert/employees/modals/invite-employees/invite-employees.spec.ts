10b5fb6ee7f30f43a6623aa5774200aec9e34105
Janusz Chwastek <janusz.chwastek@itelo.pl>
2017-10-26 14:19:57 +0200

    feature - invite employees modal

--- src/common/components/dashboard/expert/employees/modals/invite-employees/invite-employees.spec.ts
+++ src/common/components/dashboard/expert/employees/modals/invite-employees/invite-employees.spec.ts
@@ -2,46 +2,90 @@ import * as angular from 'angular'
 import IRootScopeService = profitelo.services.rootScope.IRootScopeService
 import {IExpertInviteEmployeesControllerScope, ExpertInviteEmployeesController} from './invite-employees.controller'
 import expertInviteEmployeesModule from './invite-employees'
+import {ServiceApi, InvitationApiMock} from 'profitelo-api-ng/api/api';
+import {UserService} from '../../../../../../services/user/user.service'
+import {GetService, PostInvitations} from 'profitelo-api-ng/model/models';
+
 describe('Testing Controller: expertInviteEmployeesController', () => {
 
   let expertInviteEmployeesController: ExpertInviteEmployeesController
   let scope: IExpertInviteEmployeesControllerScope
+  let $httpBackend: ng.IHttpBackendService
+  let invitationApiMock: InvitationApiMock
   const uibModalInstance = {
     dismiss: (): void => {
-
     },
     close: (): void => {
-
     }
   }
+
   beforeEach(() => {
     angular.mock.module(expertInviteEmployeesModule)
   })
 
   beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
     $provide.value('apiUrl', 'awesomeURL')
+    $provide.value('userService', UserService)
+    $provide.value('ServiceApi', ServiceApi)
   }))
 
   beforeEach(() => {
-    inject(($rootScope: IRootScopeService, $controller: ng.IControllerService) => {
+    inject(($rootScope: IRootScopeService,
+            $controller: ng.IControllerService,
+            _InvitationApiMock_: InvitationApiMock,
+            _$httpBackend_: ng.IHttpBackendService) => {
 
-      scope = <IExpertInviteEmployeesControllerScope>$rootScope.$new()
+      scope = <ng.IScope>$rootScope.$new()
+      $httpBackend = _$httpBackend_
+      invitationApiMock = _InvitationApiMock_
 
       expertInviteEmployeesController = $controller(ExpertInviteEmployeesController, {
-        '$scope': scope,
-        '$uibModalInstance': uibModalInstance
+        $scope: scope,
+        $uibModalInstance: uibModalInstance,
+        invitationApiMock
       })
     })
   })
 
-  it('should exists', () => {
-    return expect(!!expertInviteEmployeesController).toBe(true)
-  })
+  it('should exists', () => expect(!!expertInviteEmployeesController).toBe(true))
 
   it('should uibModalInstance', () => {
     spyOn(uibModalInstance, 'dismiss')
     expertInviteEmployeesController.onModalClose()
     expect(uibModalInstance.dismiss).toHaveBeenCalledWith('cancel')
+  })
+
+  it('should data in invitation for be invalid', () => {
+    expertInviteEmployeesController.invitationsInputValue = ['test@test.pl']
+    const service: GetService = {
+      id: 'id',
+      ownerId: 'ownerId',
+      name: 'name',
+      description: 'desc',
+      price: {
+        amount: 123,
+        currency: 'PLN'
+      },
+      rating: 123,
+      usageCounter: 123,
+      usageDurationInSeconds: 123,
+      language: 'pl',
+      isSuspended: false,
+      createdAt: 123,
+    }
+    expertInviteEmployeesController.onSelectedServices(service, false)
+    expertInviteEmployeesController.onSelectedServices(service, true)
+    expertInviteEmployeesController.sendInvitations()
+    expect(expertInviteEmployeesController.isSubmitted).toBe(true)
+  })
+
+  it('should send invitation', () => {
+    spyOn(expertInviteEmployeesController, 'onModalClose')
+    spyOn(expertInviteEmployeesController, 'isFormValid').and.returnValue(true)
+    invitationApiMock.postInvitationRoute(200, <PostInvitations>{})
+    expertInviteEmployeesController.sendInvitations()
+    $httpBackend.flush()
+    expect(expertInviteEmployeesController.onModalClose).toHaveBeenCalled()
   })
 
 })
