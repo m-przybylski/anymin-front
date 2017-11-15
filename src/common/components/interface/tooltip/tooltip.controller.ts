e235ea7bb81b3e71fd5a199d49ea78d1fccd920f
Janusz Chwastek <janusz.chwastek@itelo.pl>
2017-11-15 09:58:23 +0100

    fix - tooltip position on mobile

--- src/common/components/interface/tooltip/tooltip.controller.ts
+++ src/common/components/interface/tooltip/tooltip.controller.ts
@@ -1,3 +1,4 @@
+import * as angular from 'angular'
 import {ITooltipComponentBindings} from './tooltip'
 import IRootElementService = angular.IRootElementService
 
@@ -13,21 +14,25 @@ export class TooltioComponentController implements ITooltipComponentBindings {
   private tooltipContentWidth: number
   private maxTooltipWidth: number = 280
   private static readonly dividerOnHalf: number = 2
+  private static readonly maxMobilePhoneWidth: number = 480
 
   /* @ngInject */
-  constructor(private $element: IRootElementService, private $timeout: ng.ITimeoutService) {
+  constructor(private $element: IRootElementService,
+              private $timeout: ng.ITimeoutService,
+              private $window: ng.IWindowService) {
   }
 
   $onInit = (): void => {
     this.$timeout(() => {
-      this.tooltipContentWidth = this.$element.find('.tooltip-content')[0].clientWidth
-      this.tooltipStyles = {
-        left: -(this.tooltipContentWidth / TooltioComponentController.dividerOnHalf)
-      }
+      this.adjustTooltipPosition()
 
       if (this.tooltipContentWidth >= this.maxTooltipWidth) {
         this.isBigText = true
       }
+    })
+
+    angular.element(this.$window).on('resize', () => {
+      this.adjustTooltipPosition()
     })
   }
 
@@ -37,5 +42,18 @@ export class TooltioComponentController implements ITooltipComponentBindings {
 
   public onLeave = (): void => {
     this.isHover = false
+  }
+
+  private adjustTooltipPosition = (): void => {
+    this.tooltipContentWidth = this.$element.find('.tooltip-content')[0].clientWidth
+    if (this.$window.innerWidth < TooltioComponentController.maxMobilePhoneWidth) {
+      this.tooltipStyles = {
+        left: 0
+      }
+    } else {
+      this.tooltipStyles = {
+        left: -(this.tooltipContentWidth / TooltioComponentController.dividerOnHalf)
+      }
+    }
   }
 }
