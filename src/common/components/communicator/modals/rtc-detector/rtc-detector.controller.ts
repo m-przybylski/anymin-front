882c2de94a519ea43b3e560ed0e006f5125d7fba
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-11-24 11:45:22 +0100

    add positioning rtc-detector

--- src/common/components/communicator/modals/rtc-detector/rtc-detector.controller.ts
+++ src/common/components/communicator/modals/rtc-detector/rtc-detector.controller.ts
@@ -1,7 +1,24 @@
 export interface IRtcDetectorModalControllerScope extends ng.IScope {
 }
 export class RtcDetectorModalController implements ng.IController {
+  private _iconPosition: {
+    left: string,
+    top?: string
+  }
+
+  private static readonly defaultLeftPosition: string = '330px';
+  private static readonly firefoxTopPosition: string = '240px';
+  private static readonly chromeTopPosition: string = '145px';
+  private static readonly operaTopPosition: string = '190px';
+  private static readonly operaLeftPosition: string = '50%';
+
 
   /* @ngInject */
-  constructor() {}
+  constructor() {
+    this._iconPosition = {
+      left: RtcDetectorModalController.operaLeftPosition,
+      top: RtcDetectorModalController.operaTopPosition
+    }
+  }
+
 }
