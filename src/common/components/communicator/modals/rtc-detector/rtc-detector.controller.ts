2a9993c6fa933a1c0accbcb1454d0c7a026d8636
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2017-11-27 14:54:58 +0100

    handle rtc-detecotr modal for firefox and chrome and opera

--- src/common/components/communicator/modals/rtc-detector/rtc-detector.controller.ts
+++ src/common/components/communicator/modals/rtc-detector/rtc-detector.controller.ts
@@ -1,24 +1,46 @@
+const DetectRTC = require('detectrtc');
 export interface IRtcDetectorModalControllerScope extends ng.IScope {
 }
+
 export class RtcDetectorModalController implements ng.IController {
-  private _iconPosition: {
+  public iconPosition: {
     left: string,
-    top?: string
+    top?: string,
+    bottom?: string
   }
+  public isEdge = false
 
   private static readonly defaultLeftPosition: string = '330px';
   private static readonly firefoxTopPosition: string = '240px';
   private static readonly chromeTopPosition: string = '145px';
   private static readonly operaTopPosition: string = '190px';
   private static readonly operaLeftPosition: string = '50%';
-
+  private static readonly EdgeLeftPosition: string = '0px';
+  private static readonly EdgeBottomPosition: string = '120px';
 
   /* @ngInject */
   constructor() {
-    this._iconPosition = {
-      left: RtcDetectorModalController.operaLeftPosition,
-      top: RtcDetectorModalController.operaTopPosition
+    this.iconPosition = {
+      left: RtcDetectorModalController.defaultLeftPosition
+    }
+    if (DetectRTC.browser.isFirefox) {
+      this.iconPosition.top = RtcDetectorModalController.firefoxTopPosition
+    }
+    if (DetectRTC.browser.isEdge) {
+      this.isEdge = true
+      this.iconPosition = {
+        left: RtcDetectorModalController.EdgeLeftPosition,
+        bottom: RtcDetectorModalController.EdgeBottomPosition
+      }
+    }
+    if (DetectRTC.browser.isOpera) {
+      this.iconPosition = {
+        top: RtcDetectorModalController.operaTopPosition,
+        left: RtcDetectorModalController.operaLeftPosition
+      }
+    }
+    if (DetectRTC.browser.isChrome) {
+      this.iconPosition.top = RtcDetectorModalController.chromeTopPosition
     }
   }
-
 }
