a1ab8c3458f4ceecab9a8e0a84ef704c06e26392
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2018-01-08 10:19:01 +0100

    remove unnecessary get history on messanger init

--- src/angularjs/common/components/communicator/messenger/maximized/maximized.controller.ts
+++ src/angularjs/common/components/communicator/messenger/maximized/maximized.controller.ts
@@ -88,8 +88,6 @@ export class MessengerMaximizedComponentController implements ng.IController, IM
       this.callLength = data.time
       this.callCost = data.money
     })
-
-    this.getMessages(currentCall)
     this.indicateTypingDebounce = _.throttle(this.messageRoom.indicateTyping, this.indicateTypingDebounceTimeout, {
       leading: true,
       trailing: false
