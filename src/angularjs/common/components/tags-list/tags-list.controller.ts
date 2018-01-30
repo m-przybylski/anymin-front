db509b01bd33f6f9ded48ab01e42af3eb038051f
Jakub Pęksa <Jakub Pksa>
2018-01-30 14:26:58 +0100

    more/less tags button on mobile, all tags are visible on desktop now

--- src/angularjs/common/components/tags-list/tags-list.controller.ts
+++ src/angularjs/common/components/tags-list/tags-list.controller.ts
@@ -2,11 +2,23 @@ import {ITagsListBindings} from './tags-list'
 import { Tag } from 'profitelo-api-ng/model/models'
 export class TagsListComponentController implements ITagsListBindings {
 
+  public readonly defaultMobileTagsLimit: number = 7
+  private static readonly maxVisibleTags: number = 30
+
   public tags: Tag[]
-  static $inject = [];
+  public tagsLimit: number = this.defaultMobileTagsLimit
+  static $inject = []
 
   constructor() {
 
+  }
+
+  public showMoreTags = (): void => {
+    this.tagsLimit = TagsListComponentController.maxVisibleTags
+  }
+
+  public hideTags = (): void => {
+    this.tagsLimit = this.defaultMobileTagsLimit
   }
 
 }
