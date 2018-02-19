a4ac960c8310aec09b575016575dbe5b17e70974
Mariusz Bełtowski <mariusz.beltowski@itelo.pl>
2018-02-19 17:26:13 +0100

    Improve media logging

--- src/angularjs/common/services/rtc-detector/rtc-detector.ts
+++ src/angularjs/common/services/rtc-detector/rtc-detector.ts
@@ -1,10 +1,12 @@
 import * as angular from 'angular';
 import { RtcDetectorService } from './rtc-detector.service';
 import modalsModule from '../modals/modals';
+import loggerModule from '../logger/logger';
 
 const RtcDetectorModule = angular.module('profitelo.services.rtc-detector', [
   'ui.bootstrap',
-  modalsModule
+  modalsModule,
+  loggerModule
 ])
 .service('rtcDetectorService', RtcDetectorService)
   .name;
