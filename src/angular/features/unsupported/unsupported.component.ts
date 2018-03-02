af053f35d476c03f6be14476539f7ecf2bcb5eb8
Janusz Chwastek <janusz.chwastek@itelo.pl>
2018-03-02 12:32:54 +0100

    fix - unsupported ios view

--- src/angular/features/unsupported/unsupported.component.ts
+++ src/angular/features/unsupported/unsupported.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-const DetectRTC = require('detectrtc');
-
 @Component({
   selector: 'unsupported',
   templateUrl: 'unsupported.component.html',
@@ -10,10 +8,7 @@ const DetectRTC = require('detectrtc');
 
 export class UnsupportedComponent implements OnInit {
 
-  public isChromeLogoVisible: boolean;
-
   public ngOnInit(): void {
-    this.isChromeLogoVisible = DetectRTC.osName !== 'iOS' && !DetectRTC.browser.isChrome;
   }
 
 }
