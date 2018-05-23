1838ea75bda1e88bcb26185ed28cabc69170882e
Artur Wędzicha <artur.wedzicha@itelo.pl>
2018-05-23 15:12:01 +0200

    add modals

---
+++ src/app/shared/directives/pin-element/pin-element.directive.ts
@@ -0,0 +1,31 @@
+import {
+  AfterContentInit, Directive, ElementRef, HostListener, Input
+} from '@angular/core';
+
+@Directive({
+  selector: '[pinElement]'
+})
+
+export class PinElementDirective implements AfterContentInit {
+
+  private static readonly offsetTop = 48;
+  private pinElement: HTMLElement;
+
+  @Input('pinElement')
+  private pinElementClassName: string;
+
+  constructor(private element: ElementRef) {}
+
+  @HostListener('scroll', ['$event'])
+  public onElementScroll = (): void => {
+    if (this.element.nativeElement.scrollTop >= PinElementDirective.offsetTop) {
+      this.pinElement.classList.add(this.pinElementClassName + '--fixed');
+    } else {
+      this.pinElement.classList.remove(this.pinElementClassName + '--fixed');
+    }
+  }
+
+  public ngAfterContentInit(): void {
+    this.pinElement = this.element.nativeElement.querySelector('.' + this.pinElementClassName);
+  }
+}
