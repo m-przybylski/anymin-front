ec06e964478dd482e86c1b471544e698d7ec7339
Artur Wędzicha <artur.wedzicha@itelo.pl>
2018-06-12 10:02:20 +0200

    fix price bug in invite-employees modal

--- src/angularjs/common/components/dashboard/expert/employees/modals/invite-employees/invite-employees.ts
+++ src/angularjs/common/components/dashboard/expert/employees/modals/invite-employees/invite-employees.ts
@@ -1,22 +1,23 @@
 import * as angular from 'angular';
 import '../../../../../../directives/interface/pro-tags-dropdown/pro-tags-dropdown.ts';
 import { ExpertInviteEmployeesController } from './invite-employees.controller';
-import consultationListItemModule from '../../../../../invitations/consultation-list-item/consultation-list-item';
 import ValidationAlertModule from '../../../../../interface/alert/validation-alert/validation-alert';
 import inputConsultationEmployeeModule
   from '../../../../../interface/input-consultation-employee/input-consultation-employee';
 import apiModule from 'profitelo-api-ng/api.module';
 import errorHandlerModule from '../../../../../../services/error-handler/error-handler';
+import inviteEmployeeConsultationListItemModule
+    from './invite-employee-consultation-list-item/invite-employee-consultation-list-item';
 
 const expertInviteEmployeesModule = angular.module(
   'profitelo.components.dashboard.expert.activities.modals.invite-employees', [
     'ui.bootstrap',
     'profitelo.components.interface.preloader',
     inputConsultationEmployeeModule,
-    consultationListItemModule,
     ValidationAlertModule,
     errorHandlerModule,
-    apiModule
+    apiModule,
+    inviteEmployeeConsultationListItemModule
   ])
   .controller('expertInviteEmployees', ExpertInviteEmployeesController)
   .name;
