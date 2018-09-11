a2898c2e40c91d1e75f300e23c7e92c59ddaf9ab
Mariusz Bełtowski <mariusz.beltowski@anymind.com>
2018-09-11 19:12:28 +0200

    Remove briefcase config

--- src/app/shared/factories/communicator-config/communicator-config.factory.ts
+++ src/app/shared/factories/communicator-config/communicator-config.factory.ts
@@ -1,19 +1,11 @@
 // tslint:disable:only-arrow-functions
-import { Config } from 'ratel-sdk-js';
+import { UserConfig } from 'ratel-sdk-js';
 
-export function CommunicatorConfigFactory(): Config {
-
-  const ratelUrl = new URL(`${window.location.origin}/briefcase`);
+export function CommunicatorConfigFactory(): UserConfig {
   const chatUrl = new URL(`${window.location.origin}/artichoke`);
 
   return {
     logLevel: 2, // DEBUG = 0, INFO = 1, WARN = 2, ERROR = 3, NONE = 4
-    ratel: {
-      protocol: ratelUrl.protocol,
-      pathname: ratelUrl.pathname,
-      hostname: ratelUrl.hostname,
-      port: ratelUrl.port,
-    },
     chat: {
       protocol: chatUrl.protocol,
       pathname: chatUrl.pathname,
@@ -23,16 +15,18 @@ export function CommunicatorConfigFactory(): Config {
         negotiationNeededDisabled: true,
         iceTransportPolicy: 'all',
         bundlePolicy: 'max-bundle',
-        iceServers: [{
-          urls: [
-            'turn:turn.anymind.com:443?transport=udp',
-            'turn:turn.anymind.com:443?transport=tcp',
-            'stun:turn.anymind.com:443'
-          ],
-          username: 'test123',
-          credential: 'test456'
-        }]
-      }
-    }
+        iceServers: [
+          {
+            urls: [
+              'turn:turn.anymind.com:443?transport=udp',
+              'turn:turn.anymind.com:443?transport=tcp',
+              'stun:turn.anymind.com:443',
+            ],
+            username: 'test123',
+            credential: 'test456',
+          },
+        ],
+      },
+    },
   };
 }
