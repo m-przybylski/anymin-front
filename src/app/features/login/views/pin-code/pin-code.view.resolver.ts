77b9b2a63e2e2847934788bec00f4e6e7d120792
Maciej Przybylski <mprzybylski@Maciejs-MacBook-Pro.local>
2018-09-12 13:04:33 +0200

    remove reouter import

--- src/app/features/login/views/pin-code/pin-code.view.resolver.ts
+++ src/app/features/login/views/pin-code/pin-code.view.resolver.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { RegistrationService, GetRegistrationSession } from '@anymind-ng/api';
 import { Alerts, AlertService, LoggerFactory, LoggerService } from '@anymind-ng/core';
 import { catchError } from 'rxjs/operators';
