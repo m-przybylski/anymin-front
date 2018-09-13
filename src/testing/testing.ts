7bbb551803d2a8eee73c0d9b11b0442602db82f2
Maciej Przybylski <mprzybylski@Maciejs-MacBook-Pro.local>
2018-09-13 16:54:40 +0200

    fix tests for login page

--- src/testing/testing.ts
+++ src/testing/testing.ts
@@ -2,12 +2,21 @@ import { Provider } from '@angular/core';
 import { LoggerFactory, LoggerService } from '@anymind-ng/core';
 import { Deceiver } from 'deceiver-core';
 
+const mockLoggerService = Deceiver(LoggerService, {
+  warn: jasmine.createSpy('warn'),
+  debug: jasmine.createSpy('debug'),
+  error: jasmine.createSpy('error'),
+  info: jasmine.createSpy('info'),
+  log: jasmine.createSpy('log'),
+  trace: jasmine.createSpy('trace'),
+});
+
 // tslint:disable:only-arrow-functions
 export const provideMockFactoryLogger = (loggerService?: LoggerService): Provider => ({
   provide: LoggerFactory,
   useValue: Deceiver(LoggerFactory, {
     createLoggerService: jasmine
       .createSpy('')
-      .and.returnValue(typeof loggerService === 'undefined' ? Deceiver(LoggerService) : loggerService),
+      .and.returnValue(typeof loggerService === 'undefined' ? mockLoggerService : loggerService),
   }),
 });
