76dc91ba6a3262e49b77f52b05e0a46d29768f3d
Mikolaj Sikorski <mikolaj.sikorski@itelo.pl>
2018-10-16 13:26:29 +0200

    fix profile modul

--- src/app/shared/components/modals/profile/components/profile-links/profile-links.component.ts
+++ src/app/shared/components/modals/profile/components/profile-links/profile-links.component.ts
@@ -1,16 +1,11 @@
 // tslint:disable:readonly-array
 // tslint:disable:no-shadowed-variable
-import {
-  Component, EventEmitter, Input, OnInit, Output
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Animations, FormUtilsService, LoggerFactory, LoggerService } from '@anymind-ng/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { ProfileLinksComponentService } from './profile-links.component.service';
-import {
-  CommonSettingsService
-}
-  from '../../../../../../../angularjs/common/services/common-settings/common-settings.service';
+import { CommonSettingsService } from '../../../../../../../angularjs/common/services/common-settings/common-settings.service';
 
 export interface ILinkList {
   link: string;
@@ -23,11 +18,8 @@ export interface ILinkList {
   styleUrls: ['./profile-links.component.sass'],
   templateUrl: './profile-links.component.html',
   animations: Animations.addItemAnimation,
-  providers: [ProfileLinksComponentService]
-
 })
 export class ProfileLinksComponent implements OnInit {
-
   @Input()
   public form: FormGroup;
 
@@ -48,71 +40,69 @@ export class ProfileLinksComponent implements OnInit {
   private linksListUnify: string[] = [];
   private logger: LoggerService;
 
-  constructor(private formUtils: FormUtilsService,
-              private profileLinksComponentService: ProfileLinksComponentService,
-              loggerFactory: LoggerFactory,
-              CommonSettingsService: CommonSettingsService) {
+  constructor(
+    private formUtils: FormUtilsService,
+    private profileLinksComponentService: ProfileLinksComponentService,
+    loggerFactory: LoggerFactory,
+    CommonSettingsService: CommonSettingsService,
+  ) {
     this.logger = loggerFactory.createLoggerService('ProfileLinksComponent');
     this.urlPattern = CommonSettingsService.localSettings.urlPattern;
   }
 
   public ngOnInit(): void {
-    this.form.addControl(this.controlName,
-      new FormControl('', [Validators.pattern(this.urlPattern)]));
+    this.form.addControl(this.controlName, new FormControl('', [Validators.pattern(this.urlPattern)]));
     if (this.itemsList.length > 0) {
       this.itemsList.forEach(item => this.addElement(item));
     }
   }
 
-  public onChangeValue = (inputValue: string): void =>
-    this.isInputValueCorrect(inputValue)
+  public onChangeValue = (inputValue: string): void => this.isInputValueCorrect(inputValue);
 
   public isInputValueCorrect = (value: string): void => {
     if (this.form.controls[this.controlName].valid && this.urlPattern.test(value)) {
       if (this.isValueExist(this.unifyLinkProtocol(value))) {
-        this.form.controls[this.controlName]
-          .setErrors({'EDIT_PROFILE.CONTENT.CREATE_EXPERT_PROFILE.ADD_LINK.VALIDATION.VALUE_EXIST': true});
+        this.form.controls[this.controlName].setErrors({
+          'EDIT_PROFILE.CONTENT.CREATE_EXPERT_PROFILE.ADD_LINK.VALIDATION.VALUE_EXIST': true,
+        });
         this.formUtils.isFieldInvalid(this.form, this.controlName);
       } else {
         this.addElement(this.unifyLinkProtocol(value));
       }
     } else {
-      this.form.controls[this.controlName].setErrors({'VALIDATOR.ERROR.PATTERN': true});
+      this.form.controls[this.controlName].setErrors({ 'VALIDATOR.ERROR.PATTERN': true });
       this.formUtils.isFieldInvalid(this.form, this.controlName);
     }
-  }
+  };
 
   public addElement = (value: string): void => {
     if (this.profileLinksComponentService.isSocialLink(value)) {
       this.linksList.push({
         link: value,
         shortName: this.profileLinksComponentService.cropSocialMediaLinkAsName(this.unifyLinkProtocol(value)).url,
-        icon: this.profileLinksComponentService.cropSocialMediaLinkAsName(this.unifyLinkProtocol(value)).icon
+        icon: this.profileLinksComponentService.cropSocialMediaLinkAsName(this.unifyLinkProtocol(value)).icon,
       });
       this.emitItemsList();
     } else if (value.length > 0) {
-      this.linksList.push({link: value});
+      this.linksList.push({ link: value });
       this.emitItemsList();
     }
     this.clearInputValue();
-  }
+  };
 
   public onDeleteClick = (deleteItem: ILinkList): void => {
     this.linksList = this.linksList.filter(item => item !== deleteItem);
     this.emitItemsList();
-  }
+  };
 
-  public isValueExist = (value: string): boolean =>
-    this.linksList.filter(item => item.link === value).length > 0
+  public isValueExist = (value: string): boolean => this.linksList.filter(item => item.link === value).length > 0;
 
   private emitItemsList = (): void => {
     this.linksListUnify = this.linksList.map(map => map.link);
     this.linksListEmitter$.emit(this.linksListUnify);
-  }
+  };
 
-  private clearInputValue = (): void =>
-    this.form.controls[this.controlName].setValue('')
+  private clearInputValue = (): void => this.form.controls[this.controlName].setValue('');
 
-  private unifyLinkProtocol = (value: string): string =>
-    this.profileLinksComponentService.unifyLinkProtocol(value)
+  private unifyLinkProtocol = (value: string): string => this.profileLinksComponentService.unifyLinkProtocol(value);
 }
