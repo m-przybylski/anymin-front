05d1310feebd09fc09c1779bc6c30779ca1dd561
Maciej Przybylski <mprzybylski@Maciejs-MacBook-Pro.local>
2018-10-30 09:39:55 +0100

    fixes PLAT-581

--- src/app/shared/components/modals/generate-widget/services/generate-widget.data.service.ts
+++ src/app/shared/components/modals/generate-widget/services/generate-widget.data.service.ts
@@ -3,18 +3,27 @@ import { Injectable } from '@angular/core';
 import { Logger } from '@platform/core/logger';
 import { LoggerFactory, MoneyToAmount } from '@anymind-ng/core';
 import { CommonSettingsService } from 'angularjs/common/services/common-settings/common-settings.service';
-import { WidgetService, ServiceService } from '@anymind-ng/api';
+import { WidgetService, ServiceService, GetServiceWithEmployees } from '@anymind-ng/api';
 import { switchMap, map, filter } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, of, EMPTY, defer } from 'rxjs';
 import { WidgetButtonType } from '../components/generate-widget-button-type/generate-widget-button-type.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class GenerateWidgetDataService extends Logger {
+  /**
+   * This object is used to provide some data when there is noone
+   * assigned to the consultation. It may happen that there are pending
+   * invitations and none of thosee has been accepted.
+   */
+  private readonly dummyExpertName = 'GENERATE_WIDGET_MODAL.CONTENT.DUMMY_NAME';
+
   private moneyToAmount: MoneyToAmount;
   constructor(
     private commonSettingsService: CommonSettingsService,
     private widgetService: WidgetService,
     private serviceService: ServiceService,
+    private translateService: TranslateService,
     loggerFactory: LoggerFactory,
   ) {
     super(loggerFactory.createLoggerService('GenerateWidgetDataService'));
@@ -49,30 +58,59 @@ export class GenerateWidgetDataService extends Logger {
               getServiceWithEmployees => getServiceWithEmployees.serviceDetails.id === getWidget.serviceId,
             ),
           ),
-          map(getServiceWithEmployees => {
+          switchMap(getServiceWithEmployees => {
             if (getServiceWithEmployees) {
-              const tempExpertDetails = getServiceWithEmployees.employeesDetails.find(
+              const foundExpertDetails = getServiceWithEmployees.employeesDetails.find(
                 employmentWithExpertProfile => employmentWithExpertProfile.employeeProfile.id === getWidget.expertId,
               );
 
-              // expertId is not required. If expertId === undefined pick first from the list
-              const expertDetails = tempExpertDetails ? tempExpertDetails : getServiceWithEmployees.employeesDetails[0];
+              /**
+               * expertId is not required. If expertId === undefined pick first from the list
+               */
+              const expertDetails = foundExpertDetails
+                ? foundExpertDetails
+                : getServiceWithEmployees.employeesDetails.length
+                  ? getServiceWithEmployees.employeesDetails[0]
+                  : undefined;
 
-              return {
-                serviceName: getServiceWithEmployees.serviceDetails.name,
-                serviceDesc: getServiceWithEmployees.serviceDetails.description,
-                servicePrice: this.moneyToAmount.transform(getServiceWithEmployees.serviceDetails.grossPrice),
-                expertName: expertDetails.employeeProfile.name,
-                expertAvatar: expertDetails.employeeProfile.avatar,
-              };
+              return defer(
+                () =>
+                  expertDetails !== undefined
+                    ? of(
+                        this.mapServiceAndExpert(
+                          getServiceWithEmployees,
+                          expertDetails.employeeProfile.name,
+                          expertDetails.employeeProfile.avatar,
+                        ),
+                      )
+                    : this.translateService
+                        .get(this.dummyExpertName)
+                        .pipe(
+                          map((expertName: string) => this.mapServiceAndExpert(getServiceWithEmployees, expertName)),
+                        ),
+              );
             }
 
-            return undefined;
+            this.loggerService.warn(`Backend did not respond to serviceId: ${getWidget.serviceId}`);
+
+            return EMPTY;
           }),
         ),
       ),
     );
   }
+
+  private mapServiceAndExpert = (
+    getServiceWithEmployees: GetServiceWithEmployees,
+    expertName: string,
+    expertAvatar?: string,
+  ): IGenerateWidgetResolveData => ({
+    serviceName: getServiceWithEmployees.serviceDetails.name,
+    serviceDesc: getServiceWithEmployees.serviceDetails.description,
+    servicePrice: this.moneyToAmount.transform(getServiceWithEmployees.serviceDetails.grossPrice),
+    expertName,
+    expertAvatar,
+  });
 }
 
 export interface IGenerateWidgetResolveData {
