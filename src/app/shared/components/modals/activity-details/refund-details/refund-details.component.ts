00b89772ae9de151e110565f5083ab7d99c9b9aa
Janusz Chwastek <janusz.chwastek@itelo.pl>
2018-11-05 15:35:50 +0100

    feature - refund activity details

---
+++ src/app/shared/components/modals/activity-details/refund-details/refund-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { MoneyDto } from '@anymind-ng/api';
+
+export interface IRefundOperationDetails {
+  id: string;
+  date: Date;
+  refundValue: MoneyDto;
+}
+
+@Component({
+  selector: 'plat-refund-details',
+  templateUrl: './refund-details.component.html',
+  styleUrls: ['./refund-details.component.sass'],
+})
+export class RefundDetailsComponent {
+  @Input()
+  public set refundOperationDetails(value: IRefundOperationDetails | undefined) {
+    if (typeof value !== 'undefined') {
+      this.id = value.id;
+      this.date = value.date;
+      this.refundAmount = value.refundValue.amount;
+      this.refundCurrency = value.refundValue.currency;
+    }
+  }
+
+  public id: string;
+  public date: Date;
+  public refundAmount = 0;
+  public refundCurrency: string;
+}
