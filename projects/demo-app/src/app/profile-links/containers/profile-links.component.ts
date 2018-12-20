c0c01ee60d4c22f390cd506c118e43b813868446
Angular Master <maciej.przybylski@anymind.com>
2018-12-20 11:39:29 +0100

    demo project update

--- projects/demo-app/src/app/profile-links/containers/profile-links.component.ts
+++ projects/demo-app/src/app/profile-links/containers/profile-links.component.ts
@@ -1,23 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'profile-link',
   template: `
     <plat-profile-links [formControl]="links"></plat-profile-links>
-    {{ linksAr }}
+    {{ linksArray }}
   `,
 })
 export class ProfileLinkTestingComponent implements OnInit {
-  public linksAr = [];
+  public linksArray = [];
   public links = new FormControl();
-  public fg = new FormGroup({
-    links: this.links,
-  });
 
   public ngOnInit() {
     this.links.valueChanges.subscribe(links => {
-      this.linksAr = links;
+      this.linksArray = links;
     });
   }
 }
