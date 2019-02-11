764289a4e842935c83d453672cedc068abba3ad6
jakubmis <jakub.mis@anymind.com>
2019-02-11 14:13:03 +0100

    remove stage

--- gitlab-ci/configuration.sh
+++ gitlab-ci/configuration.sh
@@ -5,5 +5,4 @@ echo $G_CLOUD_SERVICE_ACCOUNT_KEY  > /etc/.gcloud-key.json
 ~/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/etc/.gcloud-key.json
 ~/google-cloud-sdk/bin/gcloud config set project dev-country-188109
 ~/google-cloud-sdk/bin/gcloud container clusters get-credentials anymind-prod-eu --zone europe-west2-a
-~/google-cloud-sdk/bin/gcloud container clusters get-credentials anymind-stage-eu --zone europe-west3-b
 ~/google-cloud-sdk/bin/gcloud container clusters get-credentials tf-anymind-stage --zone europe-west2-b
