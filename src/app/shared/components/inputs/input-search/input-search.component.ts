a5d13ce0861995a8a738c7477f6e95216fbbfdb5
Artur Wędzicha <artur.wedzicha@itelo.pl>
2019-03-06 10:09:38 +0100

    improve search input component

--- src/app/shared/components/inputs/input-search/input-search.component.ts
+++ src/app/shared/components/inputs/input-search/input-search.component.ts
@@ -2,11 +2,11 @@
 // tslint:disable:strict-boolean-expressions
 // tslint:disable:no-null-keyword
 
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { FormUtilsService } from '@anymind-ng/core';
-import { Observable, of, Subject } from 'rxjs';
-import { debounceTime, switchMap, take } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
 import { IDropdownComponent } from '@platform/shared/components/dropdown/dropdown.component';
 
 @Component({
@@ -25,54 +25,51 @@ export class InputSearchComponent implements OnInit {
   public isChangeOnSubmit = true;
   public isFocused = false;
   public isDropdownDisplayed = false;
+
   @Input()
-  public inputFormControl: FormControl;
+  public inputFormControl: FormControl = new FormControl('');
 
-  public searchInputValue: Subject<string> = new Subject();
   public suggestedSearchResults: ReadonlyArray<IDropdownComponent>;
-
-  public inviteEmployeesFormGroupName: FormGroup = new FormGroup({});
-  public inviteEmployeesControlName = 'pies';
-
-  private readonly debounceTime = 400;
+  public searchFormGroupName: FormGroup = new FormGroup({});
+  public searchControlName = 'searchControlName';
 
   constructor(public formUtils: FormUtilsService) {}
 
   public ngOnInit(): void {
-    this.searchInputValue$
-      .pipe(switchMap(searchText => this.fakeBackendRequest(searchText)))
+    this.searchFormGroupName.addControl(this.searchControlName, new FormControl('', []));
+
+    this.searchFormGroupName.controls[this.searchControlName].valueChanges
+      .pipe(switchMap(inputValue => this.fakeBackendRequest(inputValue)))
       .subscribe(suggestedSearchResult => {
         this.suggestedSearchResults = suggestedSearchResult;
         this.isDropdownDisplayed = suggestedSearchResult ? suggestedSearchResult.length !== 0 : false;
       });
   }
 
-  public get searchInputValue$(): Observable<string> {
-    return this.searchInputValue.asObservable().pipe(debounceTime(this.debounceTime));
-  }
-
-  public onSearchInputValueChange(val: string): void {
-    this.searchInputValue.next(val);
-  }
+  public onSelectItem = (value: any): void => {
+    this.searchFormGroupName.controls[this.searchControlName].setValue(value.name);
+    this.onBlur();
+  };
 
   public onBlur(): void {
-    this.inputFormControl.updateValueAndValidity();
     this.isFocused = false;
     this.isDropdownDisplayed = false;
   }
 
   public onFocus = (): void => {
-    this.inputFormControl.setErrors(null);
     this.isFocused = true;
     this.isDropdownDisplayed = this.suggestedSearchResults ? this.suggestedSearchResults.length !== 0 : false;
   };
 
-  public onEnter(): void {
-    this.onBlur();
-  }
+  public toggleDropdown = (isClickonElement: boolean): void => {
+    if (!isClickonElement) {
+      this.onBlur();
+    }
+  };
 
   private fakeBackendRequest(searchText: string): Observable<ReadonlyArray<IDropdownComponent>> {
     // TODO tutaj arturze trzeba zrobic request do backendu
-    return of([{ name: 'pies' }, { name: 'lala' }]).pipe(take(1));
+
+    return of([{ name: 'pies' }, { name: searchText }]).pipe(take(1));
   }
 }
