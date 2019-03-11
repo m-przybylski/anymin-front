1f109cf25e75f18e4a70ac06fef83b28e3d05f15
Jakub Pęksa <Jakub Pksa>
2019-03-11 13:04:11 +0100

    remove danger alert from expert dashboard view

--- src/app/features/dashboard/views/user-dashboard/expert-dashboard/expert-dashboard-consultation/expert-dashboard-consultation.component.ts
+++ src/app/features/dashboard/views/user-dashboard/expert-dashboard/expert-dashboard-consultation/expert-dashboard-consultation.component.ts
@@ -1,20 +1,13 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { EmploymentWithService } from '@anymind-ng/api';
 import { RouterHelpers, RouterPaths } from '@platform/shared/routes/routes';
-import { Alerts, AlertService, LoggerFactory } from '@anymind-ng/core';
-import { Logger } from '@platform/core/logger';
-import { Router } from '@angular/router';
-import * as fromCore from '@platform/core/reducers';
-import { Store } from '@ngrx/store';
-import { UserTypeEnum } from '@platform/core/reducers/navbar.reducer';
-import { NavbarActions } from '@platform/core/actions';
 
 @Component({
   selector: 'plat-expert-dashboard-consultations',
   templateUrl: './expert-dashboard-consultation.component.html',
   styleUrls: ['./expert-dashboard-consultation.component.sass'],
 })
-export class ExpertDashboardConsultationsComponent extends Logger {
+export class ExpertDashboardConsultationsComponent {
   @Input()
   public consultations: ReadonlyArray<EmploymentWithService>;
 
@@ -42,30 +35,11 @@ export class ExpertDashboardConsultationsComponent extends Logger {
 
   public companyProfileUrl: string;
 
-  constructor(
-    private router: Router,
-    private alertService: AlertService,
-    private store: Store<fromCore.IState>,
-    loggerFactory: LoggerFactory,
-  ) {
-    super(loggerFactory.createLoggerService('ExpertDashboardConsultationsComponent'));
-  }
-
   public add = (): void => {
     this.addConsultation.emit();
   };
 
   public onOpenConsultationDetails = (serviceId: string): void => {
     this.openConsultationDetails.emit(serviceId);
-  };
-
-  public onAlertInformationLinkClick = (): void => {
-    void this.router.navigate([this.companyProfileUrl]).then(isRedirectSuccessful => {
-      if (!isRedirectSuccessful) {
-        this.alertService.pushDangerAlert(Alerts.SomethingWentWrongWithRedirect);
-        this.loggerService.warn('Can not redirect to company profile');
-      }
-    });
-    this.store.dispatch(new NavbarActions.SetUserType(UserTypeEnum.COMPANY));
   };
 }
